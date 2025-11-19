911d92b951e9c8d4967993052e0a18144662058b
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2025-11-19 02:32:20 -0500

    0070-climbing-stairs Easy

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -6,16 +6,16 @@ public:
             return 1;
         }
 
-        vector<int> dp(n+1,0);
-
-        dp[1] = 1;
-        dp[2] = 2;
+        vector<int> a(n+1,0);
+        a[0] = 0;
+        a[1] = 1;
+        a[2] = 2;
 
         for(int i =3;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
+            a[i] = a[i-1]+a[i-2];
         }
 
-        return(dp[n]);
-
+        return(a[n]);
+        
     }
 };
