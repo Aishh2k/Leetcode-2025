1959872ee8fb79e58c347a3e01a7f3230df35a04
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2025-11-18 21:15:34 -0500

    0136-single-number Easy

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map <int, int> mp;
+
+        for(int i : nums){
+            mp[i]++;
+        }
+
+        for(auto i: mp){
+            if(i.second<2){
+                return(i.first);
+            }
+        }
+
+        return 0;
+        
+    }
+};
