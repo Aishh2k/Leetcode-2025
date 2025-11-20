835c37ba8e5d0c32a76f0a8e34344517386c08e1
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2025-11-19 19:57:29 -0500

    0739-daily-temperatures Medium

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> st;
+        int n = temperatures.size();
+        vector<int> res(n,0);
+
+        for(int i =n-1;i>=0;i--){
+            while(!st.empty() && temperatures[i]>= temperatures[st.top()]){
+                st.pop();
+            }
+
+            if(!st.empty()){
+                res[i] = st.top() - i;
+            }
+
+            st.push(i);
+            
+        }
+
+
+        return res;
+        
+    }
+};
+
+/*
+
+30 40 50 60 
+0  1. 2. 3
+
+*/
