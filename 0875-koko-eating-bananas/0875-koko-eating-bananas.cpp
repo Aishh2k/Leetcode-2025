f9913912fc42126566aa1c44a2ae56eb88c6ebe7
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2025-12-15 22:28:03 -0500

    0875-koko-eating-bananas Medium

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,36 +1,45 @@
 class Solution {
 public:
-    bool isPossible(int speed, vector<int>& piles, int h){
-        double hrs = 0;
+    bool isPossible(int k, int h , vector<int>& piles){
+
+        double count = 0;
         for(int i : piles){
-            hrs += ceil((double)i/speed);
+            count += ceil((double)i/k);
+            if(count>h){
+                return false;
+            }
         }
-        if(hrs>h){
+
+        if(count>h){
             return false;
         }else{
             return true;
         }
+
     }
+
     int minEatingSpeed(vector<int>& piles, int h) {
         int low = 1;
         int high = 0;
         for(int i: piles){
             high = max(high, i);
         }
-
-        int res;
         int mid;
+        int res;
 
         while(low<=high){
             mid = low + (high-low)/2;
-            if(isPossible(mid, piles, h)){
-                res = mid;
-                high = mid -1;
+            if(isPossible(mid,h,piles)){
+                res= mid;
+                high = mid-1;;
             }else{
                 low = mid+1;
             }
         }
 
         return res;
+        
+
+        
     }
 };
