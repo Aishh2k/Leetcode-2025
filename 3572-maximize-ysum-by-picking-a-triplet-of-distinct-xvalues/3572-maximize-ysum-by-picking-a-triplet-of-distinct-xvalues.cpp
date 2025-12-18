69911d886c80c19b09f06e2f6c35143331e2cb44
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2025-12-18 16:09:28 -0500

    3572-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues Medium

--- 3572-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/3572-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues.cpp
+++ 3572-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/3572-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     int maxSumDistinctTriplet(vector<int>& x, vector<int>& y) {
-        unordered_map<int,int> mp;
-        for(int i =0;i<x.size();i++){
-            if(!mp.count(x[i])){
-                mp[x[i]] = y[i];
-            }else{
+        unordered_map<int, int> mp;
+        for (int i = 0; i < x.size(); i++) {
+            
                 mp[x[i]] = max(y[i], mp[x[i]]);
-            }
+            
         }
 
         priority_queue<int, vector<int>, greater<int>> pq;
 
-        for(auto i: mp){
+        for (auto i : mp) {
             pq.push(i.second);
-            if(pq.size()>3){
+            if (pq.size() > 3) {
                 pq.pop();
             }
         }
 
-        if(pq.size()<3){
+        if (pq.size() < 3) {
             return -1;
-        }else{
+        } else {
             int sum = 0;
-            while(!pq.empty()){
+            while (!pq.empty()) {
                 sum += pq.top();
                 pq.pop();
             }
             return sum;
         }
-        
     }
 };
