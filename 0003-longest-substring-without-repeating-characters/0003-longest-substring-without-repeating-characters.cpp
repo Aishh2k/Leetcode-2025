497f5a3a62af20b01e2adc8545f9d610ed96ccae
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2025-12-19 13:11:04 -0500

    0003-longest-substring-without-repeating-characters Medium

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,26 +4,17 @@ public:
         unordered_set<char> st;
         int l = 0;
         int max_count = 0;
-        if (s.size() == 0) {
-            return 0;
-        }
-
-        if (s.size() < 2) {
-            return 1;
-        }
 
         for (int r = 0; r < s.size(); r++) {
-            if (!st.count(s[r])) {
-                st.insert(s[r]);
-            } else {
-                while (st.count(s[r]) && l < r) {
-                    st.erase(s[l]);
-                    l++;
-                }
-                st.insert(s[r]);
+            while (st.count(s[r])) {
+                st.erase(s[l]);
+                l++;
             }
+            
+            st.insert(s[r]);
             max_count = max(max_count, r - l + 1);
         }
+
         return max_count;
     }
 };
