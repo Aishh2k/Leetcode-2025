dc0410f612db9e383327e2300108373cbe1260b3
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2025-12-25 21:38:58 -0500

    0350-intersection-of-two-arrays-ii Easy

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> res;
+        unordered_map<int, int> mp;
+
+        for (int i : nums1) {
+            mp[i]++;
+        }
+
+        for (int i : nums2) {
+            if (mp.find(i) != mp.end()) {
+                res.push_back(i);
+                mp[i]--;
+                if(mp[i]<=0){
+                    mp.erase(i);
+                }
+            }
+        }
+
+        return res;
+    }
+};
