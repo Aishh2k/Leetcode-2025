8a67dd1e6d57f4d2d54bfa4f8225184675e384f1
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-01-04 15:40:56 -0500

    0073-set-matrix-zeroes Medium

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        bool firstRow = false;
         bool firstCol = false;
+        bool firstRow = false;
 
         for(int i =0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
+            for(int j =0;j<matrix[0].size();j++){
                 if(matrix[i][j] == 0){
-                    if(i==0){
-                        firstRow = true;
-                    }
-                    if(j == 0){
-                        firstCol = true;
-                    }
-
                     matrix[0][j] = 0;
                     matrix[i][0] = 0;
+                    if(i == 0){
+                        firstRow = true;
+                    }
+                    if(j == 0 ){
+                        firstCol = true;
+                    }
                 }
-
             }
         }
 
         for(int i =1;i<matrix.size();i++){
-            for(int j=1;j<matrix[0].size();j++){
-                if(matrix[0][j] == 0 || matrix[i][0] == 0){
+            for(int j =1;j<matrix[0].size();j++){
+                if(matrix[0][j] == 0){
                     matrix[i][j] = 0;
                 }
-
+                if(matrix[i][0] == 0){
+                    matrix[i][j] = 0;
+                }
             }
         }
 
@@ -35,11 +35,13 @@ public:
                 matrix[i][0] = 0;
             }
         }
+
         if(firstRow){
-            for(int j =0;j<matrix[0].size();j++){
-                matrix[0][j] = 0;
+            for(int i =0;i<matrix[0].size();i++){
+                matrix[0][i] = 0;
             }
         }
+
         
     }
 };
