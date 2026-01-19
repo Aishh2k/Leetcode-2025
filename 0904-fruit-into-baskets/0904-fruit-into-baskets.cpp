a9b3490846cadfc6aa8fdf50827d0c342b1cae4b
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-01-19 16:09:53 -0500

    0904-fruit-into-baskets Medium

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int l = 0;
+        unordered_map<int,int> mp;
+        int ans = 0;
+
+        for(int r = 0;r<fruits.size();r++ ){
+            mp[fruits[r]]++;
+            while(mp.size()>2 && l<fruits.size()){
+                mp[fruits[l]]--;
+                if(mp[fruits[l]]==0){
+                    mp.erase(fruits[l]);
+                }
+                l++;
+           }
+           ans = max(ans, r-l+1);
+        }
+
+
+        return ans;
+
+        
+    }
+};
