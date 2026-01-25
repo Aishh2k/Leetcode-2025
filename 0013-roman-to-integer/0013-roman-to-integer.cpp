a7a82facb381036b090551504c54987f298d1009
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-01-25 09:38:28 -0500

    0013-roman-to-integer Easy

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> mp;
+        mp['I'] = 1;
+        mp['V'] = 5;
+        mp['X'] = 10;
+        mp['L'] = 50;
+        mp['C'] = 100;
+        mp['D'] = 500;
+        mp['M'] = 1000;
+        int ans = 0;
+        int i;
+
+        for ( i = 0; i < s.size()-1;) {
+            if (mp[s[i]] < mp[s[i + 1]]) {
+                ans += mp[s[i + 1]] - mp[s[i]];
+                i = i + 2;
+            } else {
+                ans += mp[s[i]];
+                i++;
+            }
+        }
+         
+        if(i==s.size()-1){
+            ans += mp[s[i]];
+        }
+
+        return ans;
+    }
+};
