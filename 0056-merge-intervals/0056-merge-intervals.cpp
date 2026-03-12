f1a2b233ec60c2e2ff5ebd93100e943d50b43ab5
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-03-11 22:25:19 -0400

    0056-merge-intervals Medium

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if (intervals.empty()) return {};
+        vector<vector<int>> res;
 
         sort(intervals.begin(), intervals.end());
-        vector<vector<int>> res;
         vector<int> cur = intervals[0];
 
         for(int i =1;i<intervals.size();i++){
-            vector<int> next = intervals[i];
-            if(cur[1]>=next[0]){
-                cur[1] = max(cur[1], next[1]);
-                
+
+            if(cur[1]>= intervals[i][0]){
+                cur[1] = max(intervals[i][1], cur[1]);
             }else{
                 res.push_back(cur);
-                cur = next;
+                cur = intervals[i];
             }
+
         }
+
         res.push_back(cur);
         return res;
-
         
     }
 };
