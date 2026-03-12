c8d38893adb5b5921e91b62975d93cbc29dafdc5
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-03-11 21:58:23 -0400

    0121-best-time-to-buy-and-sell-stock Easy

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        
-        int profit = 0;
-        int max_profit = 0;
         int l =0;
+        int r = 1;
+        int maxProfit = 0;
 
-        for(int r =1;r<prices.size();r++){
+        while(r<prices.size()){
             if(prices[r]>prices[l]){
-                profit = prices[r]- prices[l];
-                max_profit = max(max_profit, profit);
+                maxProfit = max(maxProfit, prices[r]-prices[l]);
             }else{
-                l =r;
+                l = r;
             }
-
+            r++;
         }
 
-        return max_profit;
-
+        return maxProfit;
         
     }
 };
