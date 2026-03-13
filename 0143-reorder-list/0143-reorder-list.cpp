b9113d4d06c1e1fbd31ae8c215194382cfef8366
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-03-13 16:13:41 -0400

    0143-reorder-list Medium

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -11,27 +11,67 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
+        // brute force
 
-        ListNode* temp = head;
-        stack<ListNode*> st;
+        // ListNode* temp = head;
+        // stack<ListNode*> st;
 
-        while(temp){
-            st.push(temp);
-            temp = temp->next;
+        // while(temp){
+        //     st.push(temp);
+        //     temp = temp->next;
+        // }
+        // temp = head;
+        // int n = st.size();
+        // ListNode* top;
+
+        // for(int i =0;i<n/2;i++){
+        //     top = st.top();
+        //     st.pop();
+        //     ListNode* next = temp->next;
+        //     temp->next = top;
+        //     top->next = next;
+        //     temp = next;
+        // }
+        // temp->next = NULL;
+
+
+        // optimised
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
         }
+
+        ListNode* temp = slow->next; // reverse
+        slow->next = NULL;
+        ListNode* prev = NULL;
+        ListNode* cur = temp;
+
+        while(cur){
+            temp = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = temp;
+        }
+
         temp = head;
-        int n = st.size();
-        ListNode* top;
+        //prev has head of revered list
+        ListNode* next1;
+        ListNode* next2;
 
-        for(int i =0;i<n/2;i++){
-            top = st.top();
-            st.pop();
-            ListNode* next = temp->next;
-            temp->next = top;
-            top->next = next;
-            temp = next;
+        while(temp && prev){
+            next1 = temp->next;
+            next2 = prev->next;
+
+            temp->next = prev;
+            prev->next = next1;
+
+            temp = next1;
+            prev = next2;
         }
-        temp->next = NULL;
-        
+
     }
 };
