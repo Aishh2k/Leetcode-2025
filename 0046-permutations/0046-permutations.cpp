1307eb8b4dc2fbf27f38101f192d1f3ce36cdafd
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-03-13 02:50:46 -0400

    0046-permutations Medium

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> result;
-    void dfs(vector<int> nums,vector<int> res, unordered_set<int> st){
-        if(res.size()==nums.size()){
+    void dfs(vector<int> nums, vector<int> res, unordered_set<int> st) {
+        if (res.size() == nums.size()) {
             result.push_back(res);
             return;
         }
 
-        for(int i: st){
-            unordered_set<int> next = st;
-            res.push_back(i); 
-            next.erase(i); 
-            dfs(nums,res,next);
-            next.insert(i);
-            res.pop_back(); 
+        for (int i : nums) {
+            if(!st.count(i)){
+                res.push_back(i);
+                st.insert(i);
+                dfs(nums, res,st);
+                st.erase(i);
+                res.pop_back();
+            }
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<int> res;
         unordered_set<int> st(nums.begin(), nums.end());
 
-        dfs(nums,res,st);
+        dfs(nums, res, {});
         return result;
-        
     }
 };
