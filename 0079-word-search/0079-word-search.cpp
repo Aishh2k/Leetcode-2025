ec74dfbe470e270c2097776345119f4023b88fb3
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-03-15 02:37:46 -0400

    0079-word-search Medium

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
-    bool dfs(vector<vector<char>>& board, string word, int i, int j, int idx){
+    bool dfs(vector<vector<char>>& board, int i, int j , int idx, string word){
         if(idx == word.size()){
             return true;
         }
 
-        if(i<0 || j<0 || i>=board.size()|| j>=board[0].size() || board[i][j] != word[idx]){
+        if(i>=board.size() || j>= board[0].size() || i<0 || j<0 || board[i][j] != word[idx]){
             return false;
         }
 
-        char temp = board[i][j];
-        board[i][j] = '*'; // mark visited
 
-        bool res =  dfs(board, word, i+1, j, idx+1) || 
-                    dfs(board, word, i, j+1, idx+1) || 
-                    dfs(board, word, i-1, j, idx+1) || 
-                    dfs(board, word, i, j-1, idx+1);
+        char temp = board[i][j];
+        board[i][j] = '*';
+
+        bool res = dfs(board,i+1,j,idx+1,word) || 
+                    dfs(board,i,j+1,idx+1,word) ||
+                    dfs(board,i-1,j,idx+1,word) ||
+                    dfs(board,i,j-1,idx+1,word);
 
         board[i][j] = temp;
+
         return res;
-
     }
-
     bool exist(vector<vector<char>>& board, string word) {
-        int idx = 0;
+
         for(int i =0;i<board.size();i++){
             for(int j =0;j<board[0].size();j++){
-                if(dfs(board, word, i, j, idx)){
-                    return true;
+                if(board[i][j]==word[0]){
+                    if(dfs(board,i,j,0,word)){
+                        return true;
+                    }
                 }
             }
         }
