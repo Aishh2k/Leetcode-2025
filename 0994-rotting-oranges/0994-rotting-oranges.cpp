0963796ce71fc2b33a9c5287f86d1549b65997e7
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-03-16 04:32:01 -0400

    0994-rotting-oranges Medium

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -4,47 +4,45 @@ public:
         queue<pair<int, int>> q;
         int fresh = 0;
 
-        for(int i =0;i<grid.size();i++){
-            for(int j =0;j<grid[0].size();j++){
-                if(grid[i][j] == 2){
-                    q.push({i,j});
-                } else if(grid[i][j] == 1){
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == 2) {
+                    q.push(make_pair(i, j));
+                }else if(grid[i][j]==1){
                     fresh++;
                 }
             }
         }
 
-        int min = 0;
-        vector<pair<int, int>> dir = {{1,0}, {0,1}, {-1,0}, {0,-1}};
+        int t = 0;
+        vector<pair<int, int>> d = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
-        while(q.size()>0 && fresh>0){
+        while (!q.empty() && fresh>0) {
             int size = q.size();
-
-            for(int i = 0;i<size;i++){
-                auto [r,c] = q.front();
+            t++;
+            for (int i = 0; i < size; i++) {
+                pair<int, int> rot = q.front();
                 q.pop();
 
-                for(auto [dr,dc] : dir){
-                    dr += r;
-                    dc += c;
+                for (auto [dr, dc] : d) {
+                    dr += rot.first;
+                    dc += rot.second;
 
-                    if(dr >=0 && dc >=0 && dr< grid.size() && dc< grid[0].size()){
-                        if(grid[dr][dc] == 1){
-                            grid[dr][dc] = 2;
-                            q.push({dr,dc});
-                            fresh--;
-                        }
+                    while (dr >= 0 && dc >= 0 && dr < grid.size() &&
+                           dc < grid[0].size() && grid[dr][dc] == 1) {
+
+                        q.push({dr, dc});
+                        grid[dr][dc] = 2;
+                        fresh--;
                     }
                 }
             }
-            min++;
         }
 
-        if(fresh > 0 ){
+        if(fresh>0){
             return -1;
-        }else{
-            return min;
         }
-        
+
+        return t;
     }
 };
