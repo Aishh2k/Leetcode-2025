5dc89cb4e43e01f04657ec555cfd587cfd023458
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-03-18 03:11:13 -0400

    0045-jump-game-ii Medium

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int r =0;
+        int r=0;
         int l =0;
-        int far = 0;
-        int count = 0;
+        int res = 0;
 
-        while(r<nums.size()-1){
-            far = 0;
+        while(r<nums.size()){
+            int far = 0;
+
             for(int i =l;i<=r;i++){
-                far = max(far, i+nums[i]);
+                far = max(far,i+nums[i]);
             }
-            count++;
             l = r+1;
             r = far;
-
+            res++;
         }
-        return count;
+
+        return res;
+
+        
     }
 };
