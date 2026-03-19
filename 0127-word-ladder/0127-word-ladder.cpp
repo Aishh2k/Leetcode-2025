a64a4628fe244c5374847118edc656415b7d4592
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-03-19 03:18:36 -0400

    0127-word-ladder Hard

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> st(wordList.begin(), wordList.end());
-        queue<pair<string,int>> q;
-        q.push({beginWord,1});
-        st.erase(beginWord);
+    int ladderLength(string beginWord, string endWord,
+                     vector<string>& wordList) {
+        unordered_set<string> allowed(wordList.begin(), wordList.end());
+        queue<pair<string, int>> q;
+        q.push({beginWord, 1});
+        allowed.erase(beginWord);
 
-        while(!q.empty()){
+        while (!q.empty()) {
             auto [word, step] = q.front();
             q.pop();
-             
             if(word == endWord){
                 return step;
             }
-
-            for(int i =0;i<word.size();i++){ // for every character in word
-                char original = word[i];
-                for(char c ='a'; c<= 'z';c++){
-                    word[i] = c;
-                    if(st.count(word)){
-                        q.push({word,step+1});
-                        st.erase(word);
+            for (int i = 0; i < word.size(); i++) {
+                string next = word;
+                for (char c = 'a'; c <= 'z'; c++) {
+                    next[i] = c;
+                    if(allowed.count(next)){
+                        q.push({next, step+1});
+                        allowed.erase(next);
                     }
                 }
-                word[i] = original;
             }
         }
 
         return 0;
-        
     }
 };
