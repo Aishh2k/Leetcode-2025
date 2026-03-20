48adddde89b1666a39ce5803e6d9559e49764898
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-03-20 12:27:16 -0400

    0460-lfu-cache Hard

--- 0460-lfu-cache/0460-lfu-cache.cpp
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -11,12 +11,39 @@ struct Node {
         f = 1;
     }
 };
+
+class List{
+    public:
+        Node* head = new Node(-1,-1);
+        Node* tail = new Node(-1,-1);
+
+    List(){
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    void addNode(Node* node) {
+        Node* temp = head->next;
+        head->next = node;
+        node->prev = head;
+        node->next = temp;
+        temp->prev = node;
+    }
+
+    void deleteNode(Node* node) {
+        Node* delPrev = node->prev;
+        Node* delNext = node->next;
+        delPrev->next = delNext;
+        delNext->prev = delPrev;
+    }
+};
+
 class LFUCache {
 public:
     int minFreq;
     int maxCapacity;
     unordered_map<int, Node*> mp; // access map
-    unordered_map<int, list<Node*>> freqMap; // freq map
+    unordered_map<int, List> freqMap; // freq map
 
     LFUCache(int capacity) {
         maxCapacity = capacity;
@@ -25,9 +52,9 @@ public:
 
     void updateFreq(Node* node){
         int freq = node->f;
-        freqMap[freq].remove(node);
+        freqMap[freq].deleteNode(node);
 
-        if(freqMap[freq].empty()){
+        if(freqMap[freq].head->next == freqMap[freq].tail){
             freqMap.erase(freq);
             if(minFreq == freq){
                 minFreq++;
@@ -35,7 +62,7 @@ public:
         }
 
         node->f++;
-        freqMap[++freq].push_front(node);
+        freqMap[++freq].addNode(node);
     }
     
     int get(int key) {
@@ -57,14 +84,14 @@ public:
         }
 
         if(mp.size() == maxCapacity){ // evict LFU
-            Node* node = freqMap[minFreq].back();
-            mp.erase(node->k);
-            freqMap[minFreq].pop_back();
+            Node* node = freqMap[minFreq].tail;
+            mp.erase(node->prev->k);
+            freqMap[minFreq].deleteNode(node->prev);
         }
 
         Node* node = new Node(key, value);
         mp[key] = node;
-        freqMap[1].push_front(node);
+        freqMap[1].addNode(node);
         minFreq = 1;  
     }
 };
