cb3a8f5ae6f45c073501f6911d08a37b43f025c3
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-03-22 03:49:13 -0400

    0130-surrounded-regions Medium

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& board, int i , int j ){
-        if(i<0 || j<0 || i>=board.size() || j >=board[0].size() || board[i][j] != 'O'){
+    void dfs(int i, int j, vector<vector<char>>& board){
+        if(i<0 || j<0 || i>=board.size() || j>=board[0].size() || board[i][j] != 'O'){
             return;
         }
+
         board[i][j] = '*';
-        dfs(board, i-1, j);
-        dfs(board, i+1, j);
-        dfs(board, i, j+1);
-        dfs(board, i, j-1);
+        dfs(i+1,j,board);
+        dfs(i-1,j,board);
+        dfs(i,j-1, board);
+        dfs(i,j+1,board);
     }
     void solve(vector<vector<char>>& board) {
 
         for(int j =0;j<board[0].size();j++){
             if(board[0][j] == 'O'){
-                dfs(board, 0, j);
+                dfs(0,j,board);
             }
 
-            if(board[board.size()-1][j] == 'O'){
-                dfs(board,board.size()-1, j);
+            if(board[board.size()-1][j]=='O'){
+                dfs(board.size()-1,j,board);
             }
         }
-
         for(int i =0;i<board.size();i++){
             if(board[i][0] == 'O'){
-                dfs(board, i, 0);
+                dfs(i,0,board);
             }
 
-            if(board[i][board[0].size()-1] == 'O'){
-                dfs(board,i, board[0].size()-1);
+            if(board[i][board[0].size()-1]=='O'){
+                dfs(i,board[0].size()-1,board);
             }
         }
 
-        for(int i =0;i<board.size();i++){
+        for(int i=0;i<board.size();i++){
             for(int j =0;j<board[0].size();j++){
-                if(board[i][j] == 'O'){
-                    board[i][j] = 'X';
-                }else if(board[i][j]=='*'){
+                if(board[i][j] == '*'){
                     board[i][j] = 'O';
+                }else if(board[i][j] == 'O'){
+                    board[i][j] = 'X';
                 }
             }
         }
