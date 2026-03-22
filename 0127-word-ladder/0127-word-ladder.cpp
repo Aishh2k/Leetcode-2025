e09b2430a67d388269630d93b63361a0a4b82058
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-03-22 04:08:36 -0400

    0127-word-ladder Hard

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord,
-                     vector<string>& wordList) {
-        unordered_set<string> allowed(wordList.begin(), wordList.end());
-        queue<pair<string, int>> q;
-        q.push({beginWord, 1});
-        allowed.erase(beginWord);
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
 
-        while (!q.empty()) {
+        if(beginWord == endWord){
+            return 1;
+        }
+        unordered_set<string> st(wordList.begin(), wordList.end());
+        if(!st.count(endWord)){
+            return 0;
+        }
+
+        queue<pair<string,int>>q;
+        q.push({beginWord, 1});
+
+        while(!q.empty()){
             auto [word, step] = q.front();
             q.pop();
+
             if(word == endWord){
                 return step;
             }
-            for (int i = 0; i < word.size(); i++) {
-                string next = word;
-                for (char c = 'a'; c <= 'z'; c++) {
-                    next[i] = c;
-                    if(allowed.count(next)){
-                        q.push({next, step+1});
-                        allowed.erase(next);
+            for(int i =0;i<word.size();i++){
+                string dup = word;
+                for(char c ='a';c<='z';c++){
+                    dup[i]=c;
+                    if(st.count(dup)){
+                        q.push({dup,step+1});
+                        st.erase(dup);
                     }
                 }
             }
         }
 
-        return 0;
+    return 0;
+        
     }
 };
