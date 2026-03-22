375a842ccb6c5dcd461bcbfe25ac6c8444e3b9ea
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-03-22 01:52:44 -0400

    0200-number-of-islands Medium

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     void dfs(int i, int j, vector<vector<char>>&grid){
-        if(i>=grid.size() || j>= grid[0].size() || i<0 || j<0 || grid[i][j] != '1'){
+        if(i>=grid.size() || j>= grid[0].size() || i<0 || j<0 || grid[i][j]!='1'){
             return;
         }
-        grid[i][j] = '*';
-
+        grid[i][j] = '0';
         dfs(i+1,j,grid);
         dfs(i,j+1,grid);
         dfs(i-1,j,grid);
         dfs(i,j-1,grid);
     }
     int numIslands(vector<vector<char>>& grid) {
-        int res = 0;
+        int ans = 0;
+
         for(int i =0;i<grid.size();i++){
             for(int j =0;j<grid[0].size();j++){
-                if(grid[i][j] == '1'){
-                    res++;
+                if(grid[i][j]=='1'){
+                    ans++;
                     dfs(i,j,grid);
                 }
             }
         }
-
-        return res;
-        
+        return ans; 
     }
 };
