f8af5769fffb3cce9ea0a95fb0f25ff7c2c12c9a
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-03-22 01:59:15 -0400

    0695-max-area-of-island Medium

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    void mark(vector<vector<int>> &grid, int i, int j, int & area){
-        if(i<0 || j<0 || i>= grid.size() || j>= grid[0].size() || grid[i][j] !=1){
+    void dfs(int i,int j, int& area, vector<vector<int>>& grid){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j] != 1){
             return;
         }
 
+        grid[i][j] = 0;
         area++;
-        grid[i][j] = 9;
-        mark(grid,i+1, j, area);
-        mark(grid, i, j+1, area);
-        mark(grid, i-1, j, area);
-        mark(grid, i, j-1, area);
+        
+        dfs(i+1,j,area, grid);
+        dfs(i,j+1,area, grid);
+        dfs(i-1,j,area,grid);
+        dfs(i,j-1,area,grid);
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int maxArea = 0;
-        int area = 0;
-
+        int res = 0;
         for(int i =0;i<grid.size();i++){
-            for(int j =0;j<grid[0].size();j++){
-                if(grid[i][j] == 1){
-                    area = 0;
-                    mark(grid, i, j, area);
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==1){
+                    int area = 0;
+                    dfs(i,j,area, grid);
+                    res = max(res,area);
                 }
-                maxArea = max(maxArea,area);
             }
         }
 
-        return maxArea;
+        return res;
         
     }
 };
