a7ff1ec4e77218e14cf3dbfb2b3891cae46102a2
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-04-11 19:19:22 -0400

    0002-add-two-numbers Medium

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,33 +11,34 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int carry = 0;
-        int a, b, sum;
         ListNode* head = nullptr;
         ListNode* temp = nullptr;
-
+        int carry = 0;
         while(l1 || l2 || carry){
-            a = l1? l1->val:0;
-            b = l2? l2->val:0;
-            sum = a+b+carry;
-            carry = sum/10;
-            sum = sum%10;
-            ListNode* node  = new ListNode(sum);
-            if(!head){
-                head = node;
-                temp = node;
-            }else{
-                temp->next = node;
-                temp = node;
-            }
-
+            int a = 0;
+            int b = 0;
             if(l1){
+                a = l1->val;
                 l1 = l1->next;
             }
             if(l2){
+                b = l2->val;
                 l2 = l2->next;
             }
+
+            int sum = a+b+carry;
+            carry = sum/10;
+            ListNode* n = new ListNode(sum%10);
+
+            if(!head){
+                head = temp = n;
+            }else{
+                temp->next = n;
+                temp = temp->next;
+            }
         }
+
         return head;
+        
     }
 };
