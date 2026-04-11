93cd8f437eb744b7deda1fbc20d978e44199d887
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-04-11 19:53:04 -0400

    0007-reverse-integer Medium

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -2,23 +2,19 @@ class Solution {
 public:
     int reverse(int x) {
         int ans = 0;
-
+        
         while(x!=0){
-            int digit = x%10;
-            x = x/10;
+            int digit = x%10; //3
+            x = x/10; //12
 
-            if(ans >INT_MAX/10 || (ans == INT_MAX/10 && digit>7)){
+            if(ans > INT_MAX/10 || (ans == INT_MAX/10 && digit >7)){
+                return 0;
+            }else if(ans<INT_MIN/10 || (ans == INT_MIN/10 && digit <-8)){
                 return 0;
             }
-            if(ans<INT_MIN/10 || (ans == INT_MIN/10 && digit<-8)){
-                return 0;
-            }
-
             ans = ans*10 + digit;
-
         }
 
         return ans;
-        
     }
 };
