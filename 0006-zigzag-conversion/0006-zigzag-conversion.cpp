2107addea2eefa679c640d24e0b74f1ca3a1e1fe
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-04-11 19:44:50 -0400

    0006-zigzag-conversion Medium

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -3,18 +3,19 @@ public:
     string convert(string s, int numRows) {
         if(numRows ==1){
             return s;
-        }
 
+        }
         vector<string> rows(numRows);
-        bool goingDown = true;
         int curRow = 0;
+        bool goingDown = true;
 
         for(char c: s){
             rows[curRow] +=c;
-            if(curRow == 0){
-                goingDown = true;
-            }else if(curRow==numRows-1){
+
+            if(curRow == numRows -1){
                 goingDown = false;
+            }else if(curRow == 0){
+                goingDown = true;
             }
 
             if(goingDown){
@@ -22,14 +23,12 @@ public:
             }else{
                 curRow--;
             }
-            
         }
 
         string ans = "";
-        for(string row : rows){
-            ans += row;
+        for(string s: rows){
+            ans+=s;
         }
-
         return ans;
         
     }
