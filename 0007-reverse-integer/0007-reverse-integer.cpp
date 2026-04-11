c2171022f7d8072834361430b3b2da30e31838f0
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-04-11 19:49:45 -0400

    0007-reverse-integer Medium

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -7,10 +7,10 @@ public:
             int digit = x%10;
             x = x/10;
 
-            if(ans >INT_MAX/10 || (ans == INT_MAX/10 && digits>7)){
+            if(ans >INT_MAX/10 || (ans == INT_MAX/10 && digit>7)){
                 return 0;
             }
-            if(ans<INT_MIN/10 || (ans == INT_MIN/10 && digits<-8)){
+            if(ans<INT_MIN/10 || (ans == INT_MIN/10 && digit<-8)){
                 return 0;
             }
 
