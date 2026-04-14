502ec8c758251c0b6e9437cc41747cfb5a8d50c5
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-04-14 01:39:32 -0400

    0210-course-schedule-ii Medium

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,43 +1,49 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> graph(numCourses);
-        vector<int> inD(numCourses,0);
+        // [0,1] -> 1->0
+        // [2,1] -> 1->2
+        // num courses = 3 : 0 1 2
+        // 1 0 1
+        // [a,b] -> b->a
 
-        for(auto i: prerequisites){
-            graph[i[1]].push_back(i[0]);
-            inD[i[0]]++;
+        unordered_map<int, vector<int>> graph;
+        vector<int> inD(numCourses, 0);
+
+        for (auto p : prerequisites) {
+            int course = p[0];
+            int prereq = p[1];
+            graph[prereq].push_back(course); // if i do 1 then i can do 0, 2
+            inD[course]++;                   //
         }
 
         queue<int> q;
-        int taken = 0;
-
-        for(int i =0;i<numCourses;i++){
-            if(inD[i]==0){
+        for (int i = 0; i < numCourses; i++) {
+            if (inD[i] == 0) {
                 q.push(i);
             }
         }
+        vector<int> order;
+        int taken = 0;
 
-        vector<int> ans;
-        while(!q.empty()){
-            int course = q.front();
+        while (!q.empty()) {
+            int c = q.front();
+            order.push_back(c);
             q.pop();
             taken++;
 
-            ans.push_back(course);
-            for(int nei: graph[course]){
-                inD[nei]--;
-                if(inD[nei]==0){
-                    q.push(nei);
+            for (int i : graph[c]) {
+                inD[i]--;
+                if (inD[i] == 0) {
+                    q.push(i);
                 }
             }
         }
-        if(taken == numCourses){
-            return ans;
-        }else{
+
+        if (taken == numCourses) {
+            return order;
+        } else {
             return {};
         }
-        
-        
     }
 };
