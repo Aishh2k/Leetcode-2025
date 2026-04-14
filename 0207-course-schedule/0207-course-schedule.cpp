67851f71bbf6db248d79a8f37013da3ea1eaa85f
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-04-14 01:51:30 -0400

    0207-course-schedule Medium

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -3,9 +3,11 @@ public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         // [0,1] -> 1->0
         // [2,1] -> 1->2
+        // [0,3] -> 3->0
         // num courses = 3 : 0 1 2
-        // 1 0 1
-        // [a,b] -> b->a
+                          // 1 0 1
+
+        //q = 1
 
         unordered_map<int, vector<int>> graph;
         vector<int> inD(numCourses, 0);
@@ -14,7 +16,7 @@ public:
             int course = p[0];
             int prereq = p[1];
             graph[prereq].push_back(course); // if i do 1 then i can do 0, 2
-            inD[course]++;                   //
+            inD[course]++;                  
         }
 
         queue<int> q;
