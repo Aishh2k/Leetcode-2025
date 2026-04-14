daef9e8096d267698294c965aa515fc4b4111ea4
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-04-13 23:01:26 -0400

    0015-3sum Medium

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
-        vector<vector<int>> res;
 
-        int r;
-        int l;
-
-        for(int i =0;i<nums.size()-2;i++){
-
-            if(i>0 && nums[i] == nums[i-1]){
-                continue;
+        for(int i =0;i<nums.size();i++){
+            while(i>0 && i<nums.size() && nums[i] == nums[i-1]){
+                i++;
             }
-            l = i+1;
-            r = nums.size()-1;
+            int r = nums.size()-1;
+            int l = i+1;
 
             while(l<r){
-                int sum = nums[l]+ nums[r] + nums[i];
-                if(sum>0){
-                    r--;
-                }else if(sum<0){
-                    l++;
-                }else{
-                    res.push_back({nums[i],nums[l],nums[r]});
+                int sum = nums[r] + nums[l] + nums[i];
+                if(sum == 0){
+                    ans.push_back({nums[i],nums[l],nums[r]});
                     l++;
                     r--;
-                    while(l<r && nums[l] == nums[l-1]){
+
+                    while(l>0 && l<r && nums[l] == nums[l-1]){
                         l++;
                     }
-                    while(l<r && nums[r] == nums[r+1]){
+
+                    while(r<nums.size()-1 && l<r && nums[r] == nums[r+1]){
                         r--;
                     }
+                }else if(sum>0){
+                    r--;
+                }else{
+                    l++;
                 }
             }
         }
 
-        return res;
-
+        return ans;
         
     }
 };
