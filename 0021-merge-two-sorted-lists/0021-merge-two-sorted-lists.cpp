9e44a8b873b3276d2b11e30d7c91f47870d11161
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-04-13 21:06:13 -0400

    0021-merge-two-sorted-lists Easy

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,45 +11,49 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* temp = NULL;
+        ListNode* head = NULL;
 
-        ListNode* temp = nullptr;
-        ListNode* head = nullptr;
-
-        if (!list1) {
+        if(!list1){
             return list2;
         }
 
-        if (!list2) {
+        if(!list2){
             return list1;
         }
 
-        if (list1->val > list2->val) {
-            head = temp = list2;
-            list2 = list2->next;
-        } else {
-            head = temp = list1;
+        if(list1->val <list2->val){
+            temp = list1;
+            head = list1;
             list1 = list1->next;
+        }else{
+            temp = list2;
+            head = list2;
+            list2 = list2->next;
         }
 
-        while (list1 && list2) {
-            if (list1->val > list2->val) {
-                temp->next = list2;
-                list2 = list2->next;
-            } else {
+        while(list1 && list2){
+            if(list1->val<list2->val){
                 temp->next = list1;
                 list1 = list1->next;
+            }else{
+                temp->next = list2;
+                list2 = list2->next;
             }
-
             temp = temp->next;
         }
 
         if(list1){
             temp->next = list1;
         }
+
         if(list2){
             temp->next = list2;
         }
 
         return head;
+
+
+        
     }
 };
