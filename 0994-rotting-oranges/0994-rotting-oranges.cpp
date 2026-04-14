4fc7bb35d1f389d29dc501c8599d70909fcbc218
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-04-14 02:21:58 -0400

    0994-rotting-oranges Medium

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -7,42 +7,42 @@ public:
         for (int i = 0; i < grid.size(); i++) {
             for (int j = 0; j < grid[0].size(); j++) {
                 if (grid[i][j] == 2) {
-                    q.push(make_pair(i, j));
-                }else if(grid[i][j]==1){
+                    q.push({i, j});
+                } else if (grid[i][j] == 1) {
                     fresh++;
                 }
             }
         }
 
-        int t = 0;
-        vector<pair<int, int>> d = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        vector<vector<int>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        int m = 0;
 
-        while (!q.empty() && fresh>0) {
+        while (!q.empty() && fresh > 0) {
             int size = q.size();
-            t++;
+            m++;
+
             for (int i = 0; i < size; i++) {
-                pair<int, int> rot = q.front();
+                auto [r, c] = q.front();
                 q.pop();
 
-                for (auto [dr, dc] : d) {
-                    dr += rot.first;
-                    dc += rot.second;
+                for (auto d : dir) {
+                    int rd = r + d[0];
+                    int cd = c + d[1];
 
-                    while (dr >= 0 && dc >= 0 && dr < grid.size() &&
-                           dc < grid[0].size() && grid[dr][dc] == 1) {
-
-                        q.push({dr, dc});
-                        grid[dr][dc] = 2;
+                    if (rd >= 0 && rd < grid.size() && cd >= 0 &&
+                        cd < grid[0].size() && grid[rd][cd] == 1) {
+                        grid[rd][cd] = 2;
                         fresh--;
+                        q.push({rd, cd});
                     }
                 }
             }
         }
 
-        if(fresh>0){
+        if (fresh > 0) {
             return -1;
+        } else {
+            return m;
         }
-
-        return t;
     }
 };
