ce8c03f195774c0a7e3b97ad478e3950b6db04d5
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-04-14 01:38:37 -0400

    0207-course-schedule Medium

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,48 +1,47 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        // [0,1] -> 1->0 
+        // [0,1] -> 1->0
         // [2,1] -> 1->2
+        // num courses = 3 : 0 1 2
+        // 1 0 1
         // [a,b] -> b->a
 
-        unordered_map<int,vector<int>> graph;
-        vector<int> inD(numCourses,0);
+        unordered_map<int, vector<int>> graph;
+        vector<int> inD(numCourses, 0);
 
-        for(auto p : prerequisites){
-            graph[p[1]].push_back(p[0]); //if i do 1 then i can do 0, 2
-            inD[p[0]]++;
+        for (auto p : prerequisites) {
+            int course = p[0];
+            int prereq = p[1];
+            graph[prereq].push_back(course); // if i do 1 then i can do 0, 2
+            inD[course]++;                   //
         }
 
         queue<int> q;
-        for(int i =0;i<numCourses;i++){
-            if(inD[i] == 0){
+        for (int i = 0; i < numCourses; i++) {
+            if (inD[i] == 0) {
                 q.push(i);
             }
         }
         int taken = 0;
 
-
-        while(!q.empty()){
+        while (!q.empty()) {
             int c = q.front();
             q.pop();
             taken++;
 
-            for(int i: graph[c]){
+            for (int i : graph[c]) {
                 inD[i]--;
-                if(inD[i] == 0){
+                if (inD[i] == 0) {
                     q.push(i);
                 }
             }
         }
 
-        if(taken == numCourses){
+        if (taken == numCourses) {
             return true;
-        }else{
+        } else {
             return false;
         }
-
-
-
-        
     }
 };
