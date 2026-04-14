1b623d111edc210ae92bfd3611288531f614ee9f
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-04-14 18:57:46 -0400

    0075-sort-colors Medium

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int o =0;
+        int o = 0;
         int t = nums.size()-1;
         int i =0;
 
         while(i<nums.size() && i<=t){
             if(nums[i] == 0){
-                swap(nums[i++], nums[o++]);
-            }else if(nums[i]==2){
-                swap(nums[i], nums[t--]);
+                swap(nums[i], nums[o]);
+                o++;
+                i++;
+            }else if (nums[i] == 2){
+                swap(nums[i], nums[t]);
+                t--;
             }else{
                 i++;
             }
