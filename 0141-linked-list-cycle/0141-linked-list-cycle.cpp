4cab6efb4ee9ae5770ca3c7315f98fc6377b7142
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-04-14 18:45:44 -0400

    0141-linked-list-cycle Easy

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -12,32 +12,20 @@ public:
         if(!head || !head->next){
             return false;
         }
-        //ListNode* temp = head;
-        // unordered_set<ListNode*> st;
-        // while(temp){
-        //     if(st.count(temp)){
-        //         return true;
-        //     }
-        //     st.insert(temp);
-        //     temp = temp->next;
-        // }
-        // return false;
-
-
 
         ListNode* slow = head;
         ListNode* fast = head;
+
 
         while(fast && fast->next){
             slow = slow->next;
             fast = fast->next->next;
 
-            if(fast == slow){
+            if(slow == fast){
                 return true;
             }
         }
 
         return false;
-        
     }
 };
