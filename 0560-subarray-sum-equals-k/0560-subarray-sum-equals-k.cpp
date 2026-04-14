ba20b85343c315ee40d6dbe9a66eb2ec9c0cf6d2
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-04-13 20:19:34 -0400

    0560-subarray-sum-equals-k Medium

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int res = 0;
-        int sum = 0;
-        unordered_map<int,int> mp;
+        int count = 0;
+        unordered_map<int, int> mp;
         mp[0] = 1;
+        int sum = 0;
 
         for(int i =0;i<nums.size();i++){
             sum += nums[i];
-            int prefix = sum - k;
-
-            if(mp.find(prefix)!=mp.end()){
-                res += mp[prefix];
+            int rem = sum -k;
+            if(mp.find(rem) != mp.end()){
+                count += mp[rem];
             }
-
             mp[sum]++;
         }
 
-        return res;
-        
+        return count;  
     }
 };
