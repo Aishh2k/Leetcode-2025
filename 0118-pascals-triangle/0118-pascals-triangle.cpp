4b4c78075e26ae47a0b9481e70a9507bb8b93fdb
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-04-15 01:56:10 -0400

    0118-pascals-triangle Easy

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        if(numRows==1){
-            return {{1}};
-        }else if(numRows ==2){
-            return {{1},{1,1}};
+        if(numRows ==0){
+            return {{}};
+        }else if (numRows ==1){
+            return{{1}};
+        }else if (numRows ==2){
+            return{{1}, {1,1}};
         }
-
         vector<vector<int>> res;
         res.push_back({1});
         res.push_back({1,1});
 
-        for(int i = 2;i<numRows;i++){
+        for(int i =2;i<numRows;i++){
             vector<int> prev = res[i-1];
-            vector<int> next;
-
-            next.push_back(1);
-            for(int j =1;j<i;j++){
-                next.push_back(prev[j-1] + prev[j]);
+            vector<int> temp;
+            temp.push_back(1);
+            for(int j=0;j<i-1;j++){
+                temp.push_back(prev[j]+ prev[j+1]);
             }
-            next.push_back(1);
-            res.push_back(next);
+            temp.push_back(1);
+            res.push_back(temp);
         }
-
         return res;
-        
     }
 };
