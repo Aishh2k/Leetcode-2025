25d8a753fc87cd6c75d89fab70e2258de4ae7159
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-04-15 17:54:32 -0400

    0146-lru-cache Medium

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,79 +1,73 @@
 class Node{
     public:
-        int k;
-        int v;
-        Node* next;
-        Node* prev;
+    int k;
+    int v;
+    Node* next;
+    Node* prev;
 
-        Node(int key, int val){
-            k = key;
-            v = val;
-            next = NULL;
-            prev = NULL;
-        }
+    Node(){
+        k = -1;
+        v = -1;
+        next = NULL;
+        prev = NULL;
+    }
 };
 
-class List{
-public:
-    Node* head = new Node(-1,-1);
-    Node* tail = new Node(-1,-1);
 
-    List(){
-        head->next = tail;
-        tail->next = head;
+class LRUCache {
+public:
+    int c;
+    unordered_map<int, Node*> mp;
+    Node* head = new Node();
+    Node* tail = new Node();
+
+    LRUCache(int capacity) {
+    c = capacity; 
+    head->next = tail;
+    tail->prev = head;
     }
     void deleteNode(Node* node){
-        Node* next = node->next;
-        Node* prev = node->prev;
-        next->prev = prev;
-        prev->next = next;
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
     }
     void addNode(Node* node){
         Node* temp = head->next;
         node->next = temp;
         temp->prev = node;
-        head->next = node;
         node->prev = head;
-    }
-};
-
-class LRUCache {
-public:
-    unordered_map<int, Node*> mp;
-    List list;
-    int cap;
-    LRUCache(int capacity) {
-        cap = capacity;
+        head->next = node;
     }
     
     int get(int key) {
-        if(mp.find(key) != mp.end()){
+        if(mp.find(key)!= mp.end()){
             Node* node = mp[key];
-            list.deleteNode(node);
-            list.addNode(node);
+            deleteNode(node);
+            addNode(node);
             return(node->v);
         }
         return -1;
     }
     
     void put(int key, int value) {
-        if(mp.find(key) != mp.end()){
+        if(mp.find(key)!=mp.end()){
             Node* node = mp[key];
             node->v = value;
-            list.deleteNode(node);
-            list.addNode(node);
+            deleteNode(node);
+            addNode(node);
             return;
         }
-
-        if(mp.size()==cap){
-            Node* node = list.tail->prev;
-            list.deleteNode(node);
+        
+        if(mp.size() == c){
+            Node* node = tail->prev;
+            deleteNode(node);
             mp.erase(node->k);
         }
-
-        Node* node = new Node(key, value);
+        
+        Node* node = new Node();
+        node->k = key;
+        node->v = value;
         mp[key] = node;
-        list.addNode(node);
+        addNode(node);
     }
 };
 
