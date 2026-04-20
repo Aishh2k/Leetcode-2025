b7accb61affcccc7118297964e67c2313d8945dc
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-04-20 18:33:16 -0400

    0242-valid-anagram Easy

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<int> sCount(26, 0);
-        vector<int> tCount(26,0);
+        if(s.size()!= t.size()){
+            return false;
+        }
+        vector<int> countS(26,0);
+        vector<int> countT(26,0);
 
         for(int i =0;i<s.size();i++){
-            sCount[s[i]-'a']++;
+            countS[s[i]-'a']++;
+            countT[t[i] -'a']++;
         }
 
-        for(int i=0;i<t.size();i++){
-            tCount[t[i]-'a']++;
-        }
-
-        if(sCount==tCount){
+        if(countS == countT){
             return true;
         }else{
             return false;
