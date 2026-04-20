bbbed8333103237f6cf7476b9cafc68b186b82b6
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-04-20 19:03:15 -0400

    0042-trapping-rain-water Hard

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-
         int l =0;
         int r = height.size()-1;
         int lmax = 0;
         int rmax = 0;
+        int val =0;
         int res = 0;
-        int val;
 
         while(l<=r){
-            lmax = max(height[l], lmax);
-            rmax = max(height[r], rmax);
+            lmax = max(lmax, height[l]);
+            rmax = max(rmax, height[r]);
 
-            if(lmax<rmax){
+            if(lmax<=rmax){
                 val = lmax - height[l];
                 l++;
             }else{
                 val = rmax - height[r];
                 r--;
             }
-
             if(val>0){
                 res += val;
             }
         }
-        return res;   
+
+        return res;
+        
     }
 };
