fd82367decbf3a1152a4061935d2b91b426fcf99
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-04-20 18:55:48 -0400

    0167-two-sum-ii-input-array-is-sorted Medium

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        vector<int> res;
+        int l =0;
+        int r = numbers.size()-1;
 
-        int l = 0;
-        int r = numbers.size() - 1;
-        int sum = 0;
-
-        while (l < r) {
-            sum = numbers[l] + numbers[r];
-            if (sum == target) {
-                res.push_back(l + 1);
-                res.push_back(r + 1);
-                return res;
-            } else if (sum > target) {
+        while(l<r){
+            int sum = numbers[l] + numbers[r];
+            if(sum == target){
+                return {l+1, r+1};
+            }else if(sum>target){
                 r--;
-            } else {
+            }else{
                 l++;
             }
         }
 
-        return res;
+        return {-1,-1};
+
+        
     }
 };
