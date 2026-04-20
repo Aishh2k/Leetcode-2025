2748514de43ada3cdb729e56a7baffeae640a6e9
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-04-19 22:54:10 -0400

    0033-search-in-rotated-sorted-array Medium

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-
         int low = 0;
         int high = nums.size()-1;
-        int mid = 0;
 
         while(low<=high){
-            mid = low + (high-low)/2;
+            int mid = low + (high-low)/2;
 
             if(target == nums[mid]){
                 return mid;
-            }else if(nums[mid]>= nums[low]){ // left side is sorted
-                if(target >= nums[low] && target <= nums[mid]){ // check if target in sorted half
-                    high = mid-1;
-                }else{
-                    low = mid+1;
+            }else{ 
+                //right sorted
+                if(nums[mid]<= nums[high]){
+                    if(target> nums[mid] && target<= nums[high]){
+                        low = mid+1;
+                    }else{
+                        high = mid-1;
+                    }
+                }else{ // left sorted
+                    if(target>= nums[low] && target <nums[mid]){
+                        high = mid-1;
+                    }else{
+                        low = mid+1;
+                    }
                 }
-            }else{ // right side is sorted
-                 if(target >= nums[mid] && target <= nums[high]){ // check if target in sorted half
-                    low = mid+1;
-                }else{
-                    high = mid-1;
-                }   
             }
         }
 
