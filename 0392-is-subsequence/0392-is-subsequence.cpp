ce803d7fd81abc92ce36647c989cb84e9935ce03
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-06-03 23:14:19 -0400

    0392-is-subsequence Easy

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        if(s.size()>t.size()){
+        if (s.size() > t.size()) {
             return false;
         }
 
-        if(s.size()<1){
+        if (s.size() < 1) {
             return true;
         }
-        
+
         int j = 0;
-        for(int i =0;i<t.size();i++){
-            if(t[i] == s[j]){
+        for (int i = 0; i < t.size(); i++) {
+            if (j < s.size() && t[i] == s[j]) {
                 j++;
             }
-
-            if(j == s.size()){
-                return true;
-            }
         }
-
-        return false;
-        
+        return j == s.size();
     }
 };
