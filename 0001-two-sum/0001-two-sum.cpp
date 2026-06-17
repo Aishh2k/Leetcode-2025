ebaf11ed3fb10525e93062be5952e9b2e8eee21a
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-06-17 02:11:37 -0400

    0001-two-sum Easy

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
+        unordered_map<int,int> mp;
+        vector<int> ans;
+
         for(int i =0;i<nums.size();i++){
-            int tar = target - nums[i];
-
-            if(mp.find(tar) != mp.end()){
-                return { mp[tar], i};
+            int rem = target - nums[i];
+            if(mp.find(rem) != mp.end()){
+                ans.push_back(i);
+                ans.push_back(mp[rem]);
+                return ans;
             }
-
             mp[nums[i]] = i;
         }
 
