2eb393deb84382576935a47ca861af5d4c8833ad
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-06-17 23:36:20 -0400

    0014-longest-common-prefix Easy

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+
         sort(strs.begin(), strs.end());
 
-        string s = "";
-        int i =0;
         string a = strs[0];
         string b = strs[strs.size()-1];
+
+        string ans = "";
+        int i=0;
         while(i<a.size() && i<b.size()){
             if(a[i] == b[i]){
-                s += a[i];
-                i++;
+                ans += a[i];
             }else{
-                return s;
+                return ans;
             }
+            i++;
         }
-
-        return s;
+        return ans;
+        
     }
 };
