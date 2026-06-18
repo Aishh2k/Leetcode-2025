d63fc21fc29839e457ecc672e559a5a0dbd183f3
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-06-17 23:50:44 -0400

    0027-remove-element Easy

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int l = 0;
-        int r = 0;
-
-        while(r<nums.size()){
-            if(nums[r] != val){
-                nums[l] = nums[r];
+        for(int i =0;i<nums.size();i++){
+            if(nums[i] != val){
+                swap(nums[i], nums[l]);
                 l++;
             }
-            r++;
         }
-
         return l;
+
+        
     }
 };
