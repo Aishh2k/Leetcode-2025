36004f444fe6f7eb0eed63763e7121a526205004
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-06-17 23:53:48 -0400

    0169-majority-element Easy

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        
-        int n = nums.size();
-        unordered_map <int, int> mp;
-        for(int i : nums){
-            mp[i]++;
-        }
-        for(auto i : mp){
-            if(i.second>(n/2)){
-                return(i.first);
+        int s = nums.size();
+        unordered_map<int, int> mp;
+        for(int n: nums){
+            mp[n]++;
+            if(mp[n]> int(s/2)){
+                return n;
             }
         }
 
-        return 0;
+        for(auto i: mp){
+            if(i.second> int(s/2)){
+                return i.first;
+            }
+        }
+
+        return -1;
+        
     }
 };
