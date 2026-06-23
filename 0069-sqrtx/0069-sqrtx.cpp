b3a4066b88345fb0e2ae2ab7706c1f71dbef4703
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-06-22 23:58:58 -0400

    0069-sqrtx Easy

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int low = 0;
-        int high = x;
+        if (x <= 1) {
+            return x;
+        }
+        int l = 0;
+        int r = x;
+        int mid = 0;
+        int ans = 0;
 
-        long long mid = 0;
-        int res = 0;
-
-        while(low<=high){
-            mid = low + (high-low)/2;
-            if(mid*mid == x){
+        while (l <= r) {
+            mid = l + (r - l) / 2;
+            long long res = (long long)mid * mid;
+            if (res == x) {
                 return mid;
-            }else if(mid*mid>x){
-                high  = mid-1;
-            }else{
-                res = mid;
-                low = mid+1;
+            } else if (res > x) {
+                r = mid-1;
+            } else {
+                ans = mid;
+                l = mid + 1;
             }
         }
-
-        return res;
-        
+        return ans;
     }
 };
