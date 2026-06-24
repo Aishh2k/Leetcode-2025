800ea83850e0703daced9f8d2c1dab06c7cc7b47
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-06-24 00:40:41 -0400

    0287-find-the-duplicate-number Medium

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -3,17 +3,18 @@ public:
     int findDuplicate(vector<int>& nums) {
         int slow = 0;
         int fast = 0;
+
         do{
             slow = nums[slow];
             fast = nums[nums[fast]];
-            cout<<slow<< " : "<<fast <<endl;
-        } while(slow != fast);
-
+        }while(slow != fast);
         slow = 0;
+
         while(slow != fast){
             slow = nums[slow];
             fast = nums[fast];
         }
-        return slow;  
+        return slow;
+        
     }
 };
