849911175cfe4022426b96b34ce39842bca6f553
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-06-27 23:46:23 -0400

    0150-evaluate-reverse-polish-notation Medium

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -2,30 +2,26 @@ class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         stack<int> st;
-        for (int i = 0; i < tokens.size(); i++) {
-            if (tokens[i] == "+" || tokens[i] == "*" || tokens[i] == "-" ||
-                tokens[i] == "/") {
-                int a = st.top();
-                st.pop();
-
+        for(string s : tokens){
+            if(s == "+" || s == "-" || s =="/" || s=="*"){
                 int b = st.top();
                 st.pop();
-
-                if (tokens[i] == "+") {
-                    st.push(a + b);
-                } else if (tokens[i] == "*") {
-                    st.push(a * b);
-                } else if (tokens[i] == "/") {
-                    st.push(b/a);
-                } else {
-                    st.push(b - a);
+                int a = st.top();
+                st.pop();
+                if(s == "+"){
+                    st.push(a+b);
+                }else if(s == "-"){
+                    st.push(a-b);
+                }else if(s== "/"){
+                    st.push(a/b);
+                }else{
+                    st.push(a*b);
                 }
-
-            } else {
-                st.push(stoi(tokens[i]));
+            }else{
+                st.push(stoi(s));
             }
         }
-
         return st.top();
+        
     }
 };
