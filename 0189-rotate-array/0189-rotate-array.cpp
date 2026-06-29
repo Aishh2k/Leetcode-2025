8b97f1b4c63752d180b6f5cb42906ecf75af877c
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-06-29 01:46:24 -0400

    0189-rotate-array Medium

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
-
-    void reverseArray(vector<int>& nums, int l ,int r){
-        while(l<r){
-            swap(nums[l], nums[r]);
-            l++;r--;
-        }
-    }
     void rotate(vector<int>& nums, int k) {
-        if(nums.size()<=1){
-            return;
-        }
+        int n = nums.size();
+        k = k % n;
 
-        k = k%nums.size();
-        reverseArray(nums,0 ,nums.size()-k-1); // first 
-        reverseArray(nums, nums.size()-k, nums.size()-1); // second
-        reverseArray(nums, 0,nums.size()-1);
-        
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
     }
 };
-
