7f194c2d02cca6942510f0dee64d18894013075a
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-06-28 22:16:42 -0400

    0015-3sum Medium

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,28 +2,26 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        int l, r;
         vector<vector<int>> ans;
 
         for(int i =0;i<nums.size()-2;i++){
-            l = i+1;
-            r = nums.size()-1;
-
             if(i>0 && nums[i] == nums[i-1]){
                 continue;
             }
+            int l = i+1;
+            int r = nums.size()-1;
 
             while(l<r){
-                int sum = nums[i] + nums[l] + nums[r];
-                if(sum ==0){
+                int sum = nums[i] + nums[r] + nums[l];
+                if(sum == 0){
                     ans.push_back({nums[i], nums[l], nums[r]});
-                    l++;
                     r--;
-                    while(l<nums.size() && l<r && nums[l] == nums[l-1]){
-                        l++;
+                    l++;
+                    while(l<r && nums[r] == nums[r+1]){
+                        r--;
                     }
-                    while(r>0 && l<r && nums[r] == nums[r+1]){
-                        r--;;
+                    while(l<r && nums[l] == nums[l-1]){
+                        l++;
                     }
                 }else if(sum>0){
                     r--;
@@ -33,6 +31,5 @@ public:
             }
         }
         return ans;
-        
     }
 };
