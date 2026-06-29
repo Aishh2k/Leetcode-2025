9e9c126a33f5d3c377713744686a5d06d75dd62a
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-06-28 21:54:45 -0400

    0074-search-a-2d-matrix Medium

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int l = 0;
-        int r = matrix.size() * matrix[0].size() - 1;
-        int mid;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int low = 0;
+        int high = m*n -1;
 
-        while(l<=r){
-            mid = l + (r-l)/2;
-            int row = mid/matrix[0].size();
-            int col = mid % matrix[0].size();
-
-            if(matrix[row][col] == target){
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(matrix[mid/n][mid%n] == target){
                 return true;
-            }else if(matrix[row][col]>target){
-                r = mid-1;
+            }else if (matrix[mid/n][mid%n] > target){
+                high = mid-1;
             }else{
-                l = mid+1;
+                low = mid+1;
             }
         }
-
         return false;
         
     }
