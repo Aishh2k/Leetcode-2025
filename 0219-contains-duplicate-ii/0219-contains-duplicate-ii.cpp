035bd0ced4eab64690d182f1cd6a84250bb37085
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-06-29 21:35:28 -0400

    0219-contains-duplicate-ii Easy

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        int l = 0;
         unordered_set<int> window;
 
-        for(int r=0;r<nums.size();r++){
-            if(r-l>k){ //window too big
-                window.erase(nums[l]);
-                l++;
-            }
-            if(window.count(nums[r])){ // check in window
+        for(int i = 0;i<nums.size();i++){
+            if(window.find(nums[i]) != window.end()){
                 return true;
-            }else{
-                window.insert(nums[r]); // insert into window
             }
 
+            window.insert(nums[i]);
+
+            if(window.size() >k){
+                window.erase(nums[i-k]);
+            }
         }
 
         return false;
