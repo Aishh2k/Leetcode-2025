32616a6bac8edfc88c4c2f05ca8fdbc7888274f1
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-07-01 01:09:28 -0400

    0071-simplify-path Medium

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,35 +1,77 @@
+// class Solution {
+// public:
+//     string simplifyPath(string path) {
+//         stack<string> st;
+//         string cur = "";
+
+//         for (int i = 0; i <= path.size(); i++) {
+//             if (i == path.size() || path[i] == '/') {
+//                 if (cur.empty() || cur == ".") {
+//                     cur = "";
+//                     continue;
+//                 } else if (cur == "..") {
+//                     if (!st.empty()) {
+//                         st.pop();
+//                     }
+//                 } else {
+//                     st.push(cur);
+//                 }
+//                 cur = "";
+//             }else{
+//                 cur += path[i];
+//             }
+//         }
+//         if(st.empty()){
+//             return "/";
+//         }
+//         string ans = "";
+//         while(!st.empty()){
+//             cout<<ans<<endl;
+//             ans = "/" + st.top()+ ans;
+//             st.pop();
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     string simplifyPath(string path) {
-        stack<string> st;
+        vector<string> st;
         string cur = "";
 
         for (int i = 0; i <= path.size(); i++) {
             if (i == path.size() || path[i] == '/') {
                 if (cur.empty() || cur == ".") {
-                    cur = "";
-                    continue;
-                } else if (cur == "..") {
-                    if (!st.empty()) {
-                        st.pop();
-                    }
-                } else {
-                    st.push(cur);
+                    // ignore
                 }
+                else if (cur == "..") {
+                    if (!st.empty()) {
+                        st.pop_back();
+                    }
+                }
+                else {
+                    st.push_back(cur);
+                }
+
                 cur = "";
-            }else{
+            }
+            else {
                 cur += path[i];
             }
         }
-        if(st.empty()){
+
+        string ans = "";
+
+        for (string folder : st) {
+            ans += "/";
+            ans += folder;
+        }
+
+        if (ans == "") {
             return "/";
         }
-        string ans = "";
-        while(!st.empty()){
-            cout<<ans<<endl;
-            ans = "/" + st.top()+ ans;
-            st.pop();
-        }
+
         return ans;
     }
 };
