433cc5e62e8871ba74eeca0829fb65637778b54d
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-07-03 00:54:00 -0400

    0146-lru-cache Medium

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,29 +1,26 @@
-class Node {
+class Node{
 public:
     int k;
     int v;
     Node* next;
     Node* prev;
-
-    Node(){
-        next = NULL;
-        prev = NULL;
+    Node(int key, int value){
+        k = key;
+        v = value;
     }
 };
 
 class LRUCache {
 public:
-    int cap;
-    Node* head = new Node();
-    Node* tail = new Node();
     unordered_map<int, Node*> mp;
-
+    int cap;
+    Node* head = new Node(-1,-1);
+    Node* tail = new Node(-1,-1);
     LRUCache(int capacity) {
         cap = capacity;
         head->next = tail;
         tail->prev = head;
     }
-
     void addNode(Node* node){
         Node* temp = head->next;
         head->next = node;
@@ -31,8 +28,7 @@ public:
         temp->prev = node;
         node->prev = head;
     }
-
-    void deleteNode(Node* node){
+    void delNode(Node* node){
         node->prev->next = node->next;
         node->next->prev = node->prev;
     }
@@ -40,7 +36,7 @@ public:
     int get(int key) {
         if(mp.find(key) != mp.end()){
             Node* node = mp[key];
-            deleteNode(node);
+            delNode(node);
             addNode(node);
             return node->v;
         }
@@ -50,25 +46,21 @@ public:
     void put(int key, int value) {
         if(mp.find(key) != mp.end()){
             Node* node = mp[key];
-            deleteNode(node);
             node->v = value;
-            mp.erase(key);
+            delNode(node);
+            addNode(node);
+            return;
+        }else{
+            if(mp.size() == cap){
+                Node* del = tail->prev;
+                delNode(del);
+                mp.erase(del->k);
+            }
+
+            Node* node = new Node(key, value);
             addNode(node);
             mp[key] = node;
-            return;
         }
-
-        if(mp.size() == cap){
-            Node* node = tail->prev;
-            deleteNode(node);
-            mp.erase(node->k);
-        }
-
-        Node* node = new Node();
-        node->k = key;
-        node->v = value;
-        addNode(node);
-        mp[key] = node; 
     }
 };
 
