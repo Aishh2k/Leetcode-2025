7cd5274882fa260e0f8609aefc70dd95d44dd778
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-07-03 18:51:52 -0400

    0036-valid-sudoku Medium

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -2,42 +2,37 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
         for (int i = 0; i < 9; i++) {
-            unordered_set<int> stRow;
-            unordered_set<int> stCol;
-            unordered_set<int> stBox;
-
+            unordered_set<char> rowSet;
+            unordered_set<char> colSet;
+            unordered_set<char> boxSet;
             for (int j = 0; j < 9; j++) {
                 if (board[i][j] != '.') {
-                    if (stRow.count(board[i][j])) {
+                    if (rowSet.count(board[i][j])) { // row by row check
                         return false;
                     } else {
-                        stRow.insert(board[i][j]);
+                        rowSet.insert(board[i][j]);
                     }
                 }
-
                 if (board[j][i] != '.') {
-                    if (stCol.count(board[j][i])) {
+                    if (colSet.count(board[j][i])) { // col by col check
                         return false;
                     } else {
-                        stCol.insert(board[j][i]);
+                        colSet.insert(board[j][i]);
                     }
                 }
-                int Rstart = (i/3) *3;
-                int Cstart = (i%3) *3;
 
-                if (board[Rstart + (j/3)][Cstart + (j%3)] != '.') {
-                    if (stBox.count(board[Rstart + (j/3)][Cstart + (j%3)])) {
+                int boxRow = (i / 3) * 3 + (j / 3);
+                int boxCol = (i % 3) * 3 + (j % 3);
+                if (board[boxRow][boxCol] != '.') {
+                    if (boxSet.count(
+                            board[boxRow][boxCol])) { // box by box check
                         return false;
                     } else {
-                        stBox.insert(board[Rstart + (j/3)][Cstart + (j%3)]);
+                        boxSet.insert(board[boxRow][boxCol]);
                     }
                 }
             }
-            stRow.clear();
-            stCol.clear();
-            stBox.clear();
         }
-
         return true;
     }
 };
