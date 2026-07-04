a8d2a9dfadc96ae79425227dbe65c953d7b86f3a
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-07-03 22:36:11 -0400

    0912-sort-an-array Medium

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,13 +1,45 @@
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& nums) {
-        priority_queue<int, vector<int>, greater<int>>pq (nums.begin(), nums.end());
-        vector<int> ans;
-        while(!pq.empty()){
-            ans.push_back(pq.top());
-            pq.pop();
+    void merge(vector<int>& nums, int l, int m, int r){
+        int i = l;
+        int j = m+1;
+        vector<int> temp;
+
+        while(i<=m && j<=r){
+            if(nums[i] <= nums[j]){
+                temp.push_back(nums[i]);
+                i++;
+            }else{
+                temp.push_back(nums[j]);
+                j++;
+            }
         }
-        return ans;
-       
+
+        while(i<=m){
+            temp.push_back(nums[i]);
+            i++;
+        }
+        while(j<=r){
+            temp.push_back(nums[j]);
+            j++;
+        }
+
+        for(int k =0;k<temp.size();k++){
+            nums[l+k] = temp[k];
+        }
+    }
+    void mergeSort(vector<int>& nums, int l , int r){
+        if(l == r){
+            return;
+        }
+        int m = (l+r)/2;
+
+        mergeSort(nums, l,m);
+        mergeSort(nums,m+1, r);
+        merge(nums, l, m, r);
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums, 0, nums.size()-1);
+        return nums;
     }
 };
