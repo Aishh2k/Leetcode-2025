39410c5acf8b19f679c3af323c935807f1d745ec
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-07-05 19:51:29 -0400

    0399-evaluate-division Medium

--- 0399-evaluate-division/0399-evaluate-division.cpp
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -1,43 +1,48 @@
 class Solution {
 public:
-    double dfs(string a, string b, unordered_set<string>& visited,
-               unordered_map<string, vector<pair<string, double>>>& graph) {
-        if(!graph.count(a) || !graph.count(b)){
-            return -1.0;
-        }
-        if(a==b){
+    double dfs(string e, string f,
+               unordered_map<string, vector<pair<string, double>>>& graph,
+               unordered_set<string>& visited) {
+        if (e == f) {
             return 1.0;
         }
-        visited.insert(a);
-        for (auto [nei, val] : graph[a]) {
-            if(!visited.count(nei)){
-                double res = dfs(nei,b, visited, graph);
-                if(res != -1.0){
-                    return(val * res);
-                }
+
+        visited.insert(e);
+
+        for (auto i : graph[e]) {
+            if (visited.count(i.first)) {
+                continue;
+            }
+            double ans = dfs(i.first, f, graph, visited);
+            if (ans != -1.0) {
+                return (i.second * ans);
             }
         }
 
         return -1.0;
     }
-    vector<double> calcEquation(vector<vector<string>>& e,
+    vector<double> calcEquation(vector<vector<string>>& equations,
                                 vector<double>& values,
                                 vector<vector<string>>& queries) {
-
-        // generate the graph
         unordered_map<string, vector<pair<string, double>>> graph;
-        for (int i =0;i<e.size();i++) {
-            graph[e[i][0]].push_back({e[i][1], values[i]});
-            graph[e[i][1]].push_back({e[i][0], 1.0 / values[i]});
+
+        for (int i = 0; i < values.size(); i++) {
+            graph[equations[i][0]].push_back(
+                make_pair(equations[i][1], values[i]));
+            graph[equations[i][1]].push_back(
+                make_pair(equations[i][0], 1.0 / values[i]));
         }
 
-        //dfs traversal
-        vector<double> res;
-        for (auto i : queries) {
-            unordered_set<string> st;
-            res.push_back(dfs(i[0], i[1], st, graph));
+        vector<double> ans;
+        for (int i = 0; i < queries.size(); i++) {
+            if (!graph.count(queries[i][0]) || !graph.count(queries[i][1])) {
+                ans.push_back(-1.0);
+            } else {
+                unordered_set<string> visited;
+                ans.push_back(dfs(queries[i][0], queries[i][1], graph, visited));
+            }
         }
 
-        return res;
+        return ans;
     }
 };
