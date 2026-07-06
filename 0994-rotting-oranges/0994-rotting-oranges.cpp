e442d2ad88acf8d600e7696960d43b75af1e2e36
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-07-06 18:35:18 -0400

    0994-rotting-oranges Medium

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,48 +1,48 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<int, int>> q;
+        int time = 0;
         int fresh = 0;
+        queue<pair<int, int>> rotten;
 
         for (int i = 0; i < grid.size(); i++) {
             for (int j = 0; j < grid[0].size(); j++) {
                 if (grid[i][j] == 2) {
-                    q.push({i, j});
+                    rotten.push({i, j});
                 } else if (grid[i][j] == 1) {
                     fresh++;
                 }
             }
         }
 
-        vector<vector<int>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-        int m = 0;
+        while (!rotten.empty() && fresh > 0) {
+            int s = rotten.size();
+            time++;
 
-        while (!q.empty() && fresh > 0) {
-            int size = q.size();
-            m++;
+            for (int i = 0; i < s; i++) {
+                auto [r, c] = rotten.front();
+                rotten.pop();
 
-            for (int i = 0; i < size; i++) {
-                auto [r, c] = q.front();
-                q.pop();
+                vector<pair<int, int>> dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 
-                for (auto d : dir) {
-                    int rd = r + d[0];
-                    int cd = c + d[1];
+                for (auto [dr, dc] : dir) {
+                    dr += r;
+                    dc += c;
 
-                    if (rd >= 0 && rd < grid.size() && cd >= 0 &&
-                        cd < grid[0].size() && grid[rd][cd] == 1) {
-                        grid[rd][cd] = 2;
+                    if (dr < grid.size() && dc < grid[0].size() && dr >= 0 &&
+                        dc >= 0 && grid[dr][dc] == 1) {
                         fresh--;
-                        q.push({rd, cd});
+                        grid[dr][dc] = 2;
+                        rotten.push({dr, dc});
                     }
                 }
             }
         }
 
-        if (fresh > 0) {
+        if(fresh>0){
             return -1;
-        } else {
-            return m;
+        }else{
+            return time;
         }
     }
 };
