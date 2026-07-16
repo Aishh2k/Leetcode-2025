adf152f2049f65f38eb454fb5e87f8d09bead71e
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-07-16 18:15:29 -0400

    0752-open-the-lock Medium

--- 0752-open-the-lock/0752-open-the-lock.cpp
+++ 0752-open-the-lock/0752-open-the-lock.cpp
@@ -1,53 +1,49 @@
 class Solution {
 public:
     int openLock(vector<string>& deadends, string target) {
-        unordered_set<string> blocked (deadends.begin(), deadends.end());
-
+        unordered_set<string> blocked(deadends.begin(), deadends.end());
         if(blocked.count("0000")){
             return -1;
         }
 
-        queue<pair<string, int>>q;
+        queue<pair<string, int>> q;
         q.push({"0000", 0});
-
         unordered_set<string> visited;
         visited.insert("0000");
 
         while(!q.empty()){
             auto [state, val] = q.front();
             q.pop();
-            if(state==target){
+
+            if(state == target){
                 return val;
             }
-
-            int j;
-            for(j =0;j<4;j++){
-                if(state[j] != target[j]){
+            int i;
+            for(i =0;i<4;i++){
+                if(state[i] != target[i]){
                     break;
                 }
             }
-            
-            for(int i =j;i<state.size();i++){
+
+            for(int j =i;j<4;j++){
                 string up = state;
                 string down = state;
 
-                up[i] = (state[i] == '9')? '0': state[i]+1;
-                down[i] = (state[i] == '0')? '9': state[i]-1;
-                
+                up[j] = (state[j] == '9')? '0': state[j]+1;
+                down[j] = (state[j] == '0')? '9': state[j]-1;
+
                 if(!visited.count(up) && !blocked.count(up)){
-                    q.push({up,val+1});
+                    q.push({up, val+1});
                     visited.insert(up);
+
                 }
                 if(!visited.count(down) && !blocked.count(down)){
-                    q.push({down,val+1});
+                    q.push({down, val+1});
                     visited.insert(down);
                 }
-            } 
+            }
         }
 
         return -1;
-        
     }
 };
-
-
