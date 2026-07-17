25ace59d9bf26a9732ecc747897ce7fa95c18e59
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-07-17 03:25:15 -0400

    0684-redundant-connection Medium

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
-    vector<int> root;
-    int find(int a) {
+    int find(int a, vector<int>& root) {
         if (root[a] != a) {
-            root[a] = find(root[a]);
+            root[a] = find(root[a], root);
         }
         return root[a];
     }
+    bool unite(int a, int b, vector<int>& root) {
+        int ra = find(a, root);
+        int rb = find(b, root);
+
+        if (ra == rb) {
+            return false;
+        } else {
+            root[rb] = ra;
+            return true;
+        }
+    }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        for (int i = 0; i <= edges.size(); i++) {
-            root.push_back(i);
+        // union find algorithm
+        vector<int> root(edges.size() + 1);
+        vector<int> ans;
+        for (int i = 0; i < edges.size(); i++) {
+            root[i] = i;
         }
 
-        for (auto i : edges) {
-            int a = i[0];
-            int b = i[1];
-
-            int ra = find(a);
-            int rb = find(b);
-
-            if (ra == rb) {
-                return i;
-            } else {
-                root[rb] = ra;
+        for (auto edge : edges) {
+            if (!unite(edge[0], edge[1], root)) {
+                ans.push_back(edge[0]);
+                ans.push_back(edge[1]);
             }
         }
-        return {-1,-1};
+
+        return ans;
     }
 };
