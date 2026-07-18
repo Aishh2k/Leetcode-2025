b447317a98df8fff0611c4753221054abd5cc4fe
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-07-18 01:42:03 -0400

    0127-word-ladder Hard

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-
-        if(beginWord == endWord){
-            return 1;
-        }
         unordered_set<string> st(wordList.begin(), wordList.end());
         if(!st.count(endWord)){
             return 0;
         }
 
-        queue<pair<string,int>>q;
+        queue<pair<string, int>> q;
         q.push({beginWord, 1});
 
         while(!q.empty()){
-            auto [word, step] = q.front();
+            auto [word, val] = q.front();
             q.pop();
 
             if(word == endWord){
-                return step;
+                return val;
             }
-            for(int i =0;i<word.size();i++){
-                string dup = word;
-                for(char c ='a';c<='z';c++){
-                    dup[i]=c;
-                    if(st.count(dup)){
-                        q.push({dup,step+1});
-                        st.erase(dup);
+
+            for(int i = 0;i<word.size();i++){
+                string temp = word;
+                for(int j =0;j<26;j++){
+                    temp[i] = 'a' + j;
+                    if(st.count(temp)){
+                        st.erase(temp);
+                        q.push({temp, val+1});
                     }
                 }
             }
         }
-
-    return 0;
+        return 0;
+        
         
     }
 };
