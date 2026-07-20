0b9211d6a0866e0ecbdf2c425f267b414cd7026b
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-07-20 08:41:35 -0400

    1834-single-threaded-cpu Medium

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -2,30 +2,35 @@ class Solution {
 public:
     vector<int> getOrder(vector<vector<int>>& tasks) {
         vector<vector<int>> T;
-        for(int i =0;i<tasks.size();i++){
-            T.push_back({tasks[i][0], tasks[i][1], i});
+        int n = tasks.size();
+
+        for(int i =0;i<n;i++){
+            int start_time = tasks[i][0];
+            int proc_time = tasks[i][1];
+
+            T.push_back({start_time, proc_time, i});
         }
         long long cur_time = 0;
-        sort(T.begin(), T.end()); // sorted based off the start Time of tasks
+        int idx = 0;
+        sort(T.begin(), T.end());
+
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         vector<int> res;
 
-        int idx =0;
-        while(idx<T.size() || !pq.empty()){
-            if(pq.empty() && cur_time<T[idx][0]){
+        while(idx<n || !pq.empty()){
+            if(pq.empty() && cur_time<T[idx][0]){ //idle cur time is 5 and next availabel task is starting at 20, then you wanna jump to 20
                 cur_time = T[idx][0];
             }
-            while(idx<T.size() && T[idx][0] <= cur_time){
+
+            while(idx<n && T[idx][0] <= cur_time){
                 pq.push({T[idx][1], T[idx][2]});
                 idx++;
             }
-
-            auto [dur, pos] = pq.top();
+            auto[dur, pos] = pq.top();
             pq.pop();
             cur_time += dur;
-            res.push_back({pos});
+            res.push_back(pos);
         }
-
         return res;
     }
 };
