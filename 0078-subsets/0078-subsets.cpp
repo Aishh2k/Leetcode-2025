73bce8594b9698ba91129217e87a6509eded6f49
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-07-23 01:27:06 -0400

    0078-subsets Medium

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void dfs(int i, vector<int>& nums, vector<int>temp, vector<vector<int>>&res){
+    void dfs(int i, vector<int>& nums, vector<int>&temp, vector<vector<int>>&res){
         if(i == nums.size()){
             res.push_back(temp);
             return;
@@ -9,6 +9,7 @@ public:
         dfs(i+1, nums, temp, res);
         temp.push_back(nums[i]);
         dfs(i+1, nums, temp, res);
+        temp.pop_back();
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> res;
