a300b786997057382420e13dff6e46c895a8dc90
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-07-26 00:33:27 -0400

    0023-merge-k-sorted-lists Hard

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,71 +10,60 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwo(ListNode* l1, ListNode* l2) {
-        if (!l1)
-            return l2;
-        if (!l2)
-            return l1;
+    ListNode* merge(ListNode* L1, ListNode* L2){
+        if(!L1){
+            return L2;
+        }
+        if(!L2){
+            return L1;
+        }
 
-        ListNode* temp = NULL;
-        ListNode* head = NULL;
+        ListNode* dummy = new ListNode();
+        ListNode* temp = dummy;
 
-        while (l1 && l2) {
-            if (l1->val <= l2->val) {
-                if (!head) {
-                    head = l1;
-                    temp = l1;
-                }else{
-                    temp->next = l1;
-                    temp = temp->next;
-                }
-                l1 = l1->next;
-            } else {
-                if (!head) {
-                    head = l2;
-                    temp = l2;
-                }else{
-                    temp->next = l2;
-                    temp = temp->next;
-                }
-                l2 = l2->next;
+        while(L1 && L2){
+            if(L1->val > L2->val){
+                temp->next = L2;
+                L2 = L2->next;
+            }else{
+                temp->next = L1;
+                L1 = L1->next;
             }
+            temp = temp->next;
         }
 
-        if(l1){
-            temp->next = l1;
+        if(L1){
+            temp->next = L1;
         }
-        if(l2){
-            temp->next = l2;
+        if(L2){
+            temp->next = L2;
         }
 
-        return head;
+        return dummy->next;
     }
     ListNode* partition(int start, int end, vector<ListNode*>& lists){
         if(start == end){
             return lists[start];
         }
-
-        if(start>end){
+        if(start > end){
             return nullptr;
         }
 
         int mid = start + (end-start)/2;
+        ListNode* L1 = partition(start, mid, lists);
+        ListNode* L2 = partition(mid+1, end, lists);
 
-        ListNode* L1 = partition(start, mid, lists); //La
-        ListNode* L2 = partition(mid+1, end, lists); //Lb
+        return(merge(L1, L2));
 
-        return(mergeTwo(L1,L2));
     }
-
-    
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-
-        if(lists.size()==0){
+        if(lists.size() == 0){
             return nullptr;
         }
+        if(lists.size() == 1){
+            return lists[0];
+        }
 
-        return(partition(0,lists.size()-1, lists));
-
+        return partition(0, lists.size()-1, lists);
     }
 };
