490e9238487fac31a7ffde0cbeade9c039fe420d
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-08-02 17:07:15 -0400

    0053-maximum-subarray Medium

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int curSum = nums[0];
-        int res = nums[0];
+        int sum = nums[0];
+        int ans = nums[0];
 
-        for (int i = 1; i < nums.size(); i++) {
-            curSum = max(nums[i], curSum + nums[i]);
-            if (curSum > res) {
-                res = curSum;
+        for(int i =1;i<nums.size();i++){
+            if(sum + nums[i] > nums[i] ){
+                sum += nums[i];
+            }else{
+                sum = nums[i];
             }
+            ans = max(ans, sum);
         }
 
-        return res;
+        return ans;
+        
     }
 };
