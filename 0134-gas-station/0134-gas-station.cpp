e55f410f748642bfcc2469db80cc667000bb18c4
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-08-03 20:09:07 -0400

    0134-gas-station Medium

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int total = 0;
         int sum = 0;
         int idx = 0;
+        int total = 0;
 
-        for (int i = 0; i < cost.size(); i++) {
+        for(int i =0;i<gas.size();i++){
             sum += gas[i] - cost[i];
             total += gas[i] - cost[i];
 
             if(sum<0){
-                sum = 0;
                 idx = i+1;
+                sum = 0;
             }
         }
 
         if(total<0){
             return -1;
+        }else{
+            return idx;
         }
-        return idx;
+        
     }
 };
