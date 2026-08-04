7ed034e9cb8d03332ba24313b406dc9fb22f4624
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-08-03 20:03:08 -0400

    0846-hand-of-straights Medium

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-        int n = hand.size();
-
-        if (n % groupSize != 0) {
+        if(hand.size() % groupSize != 0){
             return false;
         }
-
         unordered_map<int, int> mp;
+        for(int i: hand){
+            mp[i]++;
+        }
         sort(hand.begin(), hand.end());
 
-        for (int i = 0; i < n; i++) {
-            mp[hand[i]]++;
-        }
+        for(int i =0;i<hand.size();i++){
+            if(mp[hand[i]] <=0){
+                continue;
+            }
 
-        for (int j = 0; j < n; j++) {
-            int cur = hand[j];
-            if (mp[cur]!= 0) {
-                for (int i = 0; i < groupSize; i++) {
-                    int target = cur + i;
-
-                    if(mp[target]==0){
-                        return false;
-                    }else{
-                        mp[target]--;
-                    }
-                }
+            int start = hand[i];
+            for(int j =0;j<groupSize;j++){
+                int t = start +j;
+                if(mp[t] <= 0)
+                    return false;
+                
+                mp[t]--;
             }
         }
-
         return true;
     }
 };
