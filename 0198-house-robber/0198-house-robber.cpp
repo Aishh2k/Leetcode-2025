97f0c7edd989f5b0b77426d16723899949ebde40
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-08-06 19:06:33 -0400

    0198-house-robber Medium

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        vector<int> arr(nums.size()+1, 0);
-        // arr[i] = max stolen money till i house
-
-        arr[1] = nums[0]; // only one house
-
-        for(int i =2;i<=nums.size();i++){
-            int steal = nums[i-1] + arr[i-2];
-            int skip = arr[i-1];
-
-            arr[i] = max(steal, skip);
+        if(nums.size() == 1){
+            return nums[0];
         }
 
-        return arr[nums.size()];
+        vector<int> dp(nums.size(), 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for(int i =2;i<nums.size();i++){
+            int steal = nums[i] + dp[i-2];
+            int skip = dp[i-1];
+
+            dp[i] = max(steal, skip);
+        }
+
+        return dp[nums.size()-1];
     }
 };
-
-//arr[0,1,0,0,0]
-//nums [1,2,3,1]
