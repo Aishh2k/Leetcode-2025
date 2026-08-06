469448d534fcc4d00b20dfd5336d863d89b69eca
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-08-06 18:11:42 -0400

    0509-fibonacci-number Easy

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
-
     int fib(int n) {
-        if(n == 0){
-            return 0;
+        if(n<=1){
+            return n;
         }
-        if(n ==1){
-            return 1;
+        vector<int> fi(n+1,0);
+        fi[1] = 1;
+
+        for(int i =2;i<=n;i++){
+            fi[i] = fi[i-1] + fi[i-2];
         }
 
-        return fib(n-1) + fib(n-2);
+        return fi[n];
+        
     }
 };
