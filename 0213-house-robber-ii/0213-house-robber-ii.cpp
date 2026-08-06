48f0fc75994ec4788c4aebb3fae66818d9aebdf1
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-08-06 19:32:28 -0400

    0213-house-robber-ii Medium

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -19,20 +19,13 @@ public:
 
     }
     int rob(vector<int>& nums) {
-
-        if(nums.size() == 1){
+        int n = nums.size();
+        if(n == 1){
             return nums[0];
-        }else if(nums.size() == 2){
-            return max(nums[0], nums[1]);
         }
-        
-        vector<int> takeFirst(nums.size()-1);
-        vector<int> skipFirst(nums.size()-1);
 
-        for(int i =0;i<nums.size()-1;i++){ // creating two subarrays
-            takeFirst[i] = nums[i];
-            skipFirst[i] = nums[i+1];
-        }
+        vector<int> takeFirst(nums.begin(), nums.end()-1);
+        vector<int> skipFirst(nums.begin()+1, nums.end());
 
         return max(helper(takeFirst), helper(skipFirst));
     }
