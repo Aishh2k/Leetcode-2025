79335931708f5f588923f8e5bcc43390e60ffe4b
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-08-06 21:50:06 -0400

    1911-maximum-alternating-subsequence-sum Medium

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     long long maxAlternatingSum(vector<int>& nums) {
-        long long answer = nums[0];
+        vector<vector<long long>> dp(nums.size(), vector<long long> (2,0));
 
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] > nums[i - 1]) {
-                answer += nums[i] - nums[i - 1];
-            }
+        dp[0][0] = 0;
+        dp[0][1] = nums[0];
+
+        for(int i =1;i<nums.size();i++){
+            // even length substring
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] - nums[i]);
+
+
+            /// odd length substring
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] + nums[i]);
         }
 
-        return answer;
+        return (max(dp[nums.size()-1][0], dp[nums.size()-1][1]));
+        
     }
 };
