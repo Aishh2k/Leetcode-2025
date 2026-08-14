7d8236c2c96a97c74ed4236297c32bf88d39c25a
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-08-14 07:35:24 -0400

    0040-combination-sum-ii Medium

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> result;
-    void dfs(int idx, vector<int>& nums, int target, int sum, vector<int>& temp){
-        if(sum == target){
+    void dfs(int idx, int target, vector<int>& temp,vector<int>&c){
+        if(target == 0){
             result.push_back(temp);
             return;
         }
 
-        for(int i = idx;i<nums.size();i++){
-            if(i>idx && nums[i] == nums[i-1]){
+        for(int i = idx;i<c.size();i++){
+            if(i > idx && c[i] == c[i-1]){
                 continue;
             }
-
-            if(sum + nums[i] <= target){
-                temp.push_back(nums[i]);
-                dfs(i+1, nums, target, sum + nums[i], temp);
+            if(target - c[i] >=0){
+                temp.push_back(c[i]);
+                dfs(i+1, target - c[i], temp, c);
                 temp.pop_back();
             }
         }
+
     }
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
-        int sum = 0;
+    vector<vector<int>> combinationSum2(vector<int>& c, int target) {
+        sort(c.begin(), c.end());
         vector<int> temp;
-        dfs(0, candidates,target, sum, temp);
+        dfs(0,target, temp, c);
         return result;
     }
 };
