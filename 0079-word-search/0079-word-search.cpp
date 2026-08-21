05c8cb7b72906ab0d564596c500ed25c1063df4b
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-08-21 00:10:12 -0400

    0079-word-search Medium

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    bool dfs(int i, int j, vector<vector<char>>& board, string word, int idx){
+    bool dfs(int i, int j, int idx, string word, vector<vector<char>>& board){
         if(idx == word.size()){
             return true;
         }
-
-        if(i>=board.size() || j>= board[0].size() || i<0 || j<0 || board[i][j] != word[idx]){
+        if(i>= board.size() || j>= board[0].size() || i<0 || j<0 || board[i][j] != word[idx]){
             return false;
         }
-
-        char temp = board[i][j];
+        
+        char c = board[i][j];
         board[i][j] = '*';
-        bool present = dfs(i+1,j, board,word, idx+1) || dfs(i,j+1, board,word, idx+1) || dfs(i,j-1, board,word, idx+1)
-        || dfs(i-1,j, board,word, idx+1);
 
-        board[i][j] = temp;
+        bool ret = dfs(i+1, j, idx+1, word, board) || dfs(i, j+1, idx+1, word, board) || dfs(i-1, j, idx+1, word, board) || dfs(i, j-1, idx+1, word, board);
 
-        return present;
+        board[i][j] = c;
+        return ret;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        int idx = 0;
-
         for(int i =0;i<board.size();i++){
-            for(int j =0;j<board[0].size();j++){
-                if(board[i][j] == word[idx]){
-                    bool present = dfs(i,j,board,word, idx);
-                    if(present){
+            for(int j = 0;j<board[0].size();j++){
+                if(board[i][j] == word[0]){
+                    bool ans = dfs(i, j, 0, word,board);
+                    if(ans){
                         return true;
                     }
                 }
