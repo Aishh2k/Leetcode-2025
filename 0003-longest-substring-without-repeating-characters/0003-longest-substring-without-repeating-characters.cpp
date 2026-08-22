d241453c8e70a0a19e36fbaa90529be87b2f2996
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-08-22 11:42:16 -0400

    0003-longest-substring-without-repeating-characters Medium

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
 
-        if(s.size()<=1){
+        if (s.size() <= 1) {
             return s.size();
         }
         int l = 0;
@@ -11,20 +11,17 @@ public:
         int ans = 0;
         unordered_set<char> st;
 
-        while(r<s.size()){
-            if(st.count(s[r])){
-                while(st.count(s[r])){
-                    st.erase(s[l]);
-                    l++;
-                }
+        while (r < s.size()) {
+            while (st.count(s[r])) {
+                st.erase(s[l]);
+                l++;
             }
 
             st.insert(s[r]);
-            ans = max(ans, r-l+1);
+            ans = max(ans, r - l + 1);
             r++;
         }
-        
-        ans = max(ans, r-l);
+
         return ans;
     }
 };
