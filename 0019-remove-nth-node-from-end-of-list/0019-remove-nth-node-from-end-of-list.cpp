f9e4edf0a21b51c97458fc72f25a825e6bd5fffe
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-08-23 11:52:36 -0400

    0019-remove-nth-node-from-end-of-list Medium

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -15,22 +15,25 @@ public:
             return head;
         }
 
-        ListNode* dummy = new ListNode();
-        dummy->next = head;
-        ListNode* fast = dummy;
-        ListNode* slow = dummy;
+        ListNode* fast = head;
+        ListNode* slow = head;
 
-        for(int i = 0;i<=n;i++){
+        for(int i = 1;i<=n;i++){
             fast = fast->next;
         }
-        while(fast){
+
+        if(fast == nullptr){
+            return head->next;
+        }
+
+        while(fast->next){
             slow = slow->next;
             fast = fast->next;
         }
 
         slow->next = slow->next->next;
 
-        return dummy->next;
+        return head;
 
     }
 };
