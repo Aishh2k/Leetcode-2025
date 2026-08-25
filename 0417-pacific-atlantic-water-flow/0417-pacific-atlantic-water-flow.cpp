cd76a65dc5fdfde4e537fa534a73a64da292c11d
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-08-25 10:26:12 -0400

    0417-pacific-atlantic-water-flow Medium

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,50 +1,44 @@
 class Solution {
 public:
-    vector<pair<int, int>> dir = {{1,0}, {0,1}, {-1,0}, {0,-1}};
-
-    void dfs(int i , int j, int prev, vector<vector<bool>>& visited, vector<vector<int>>& heights){
-        if(i>=heights.size() || j>= heights[0].size() || i<0 || j<0){
+    void dfs(int i , int j , int prevHeight, vector<vector<bool>>& vis, vector<vector<int>>& heights){
+        if(i<0 || j<0 || i>= heights.size() || j >= heights[0].size() || heights[i][j] < prevHeight || vis[i][j] != false){
             return;
         }
 
-        if(heights[i][j] < prev || visited[i][j]){
-            return;
-        }
+        vis[i][j] = true;
 
-        visited[i][j] = true;
+        dfs(i+1, j, heights[i][j], vis, heights);
+        dfs(i, j+1, heights[i][j], vis, heights);
+        dfs(i-1, j, heights[i][j], vis, heights);
+        dfs(i, j-1, heights[i][j], vis, heights);
 
-        for(auto [dr, dc]: dir){
-            dr += i;
-            dc += j;
-            dfs(dr,dc,heights[i][j], visited, heights);
-        }
     }
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         int m = heights.size();
         int n = heights[0].size();
-
         vector<vector<int>> ans;
-
         vector<vector<bool>> atl(m, vector<bool>(n, false));
         vector<vector<bool>> pac(m, vector<bool>(n, false));
 
-        for(int j=0;j<n;j++){
-            dfs(m-1,j,INT_MIN,atl,heights);
-            dfs(0, j, INT_MIN,pac, heights);
+
+        for(int i = 0;i<m;i++){
+            dfs(i,0, 0, pac, heights);
+            dfs(i, n-1, 0, atl, heights);
         }
 
-        for(int i=0;i<m;i++){
-            dfs(i,0,INT_MIN,pac,heights);
-            dfs(i,n-1,INT_MIN,atl,heights);
+        for(int j = 0;j<n;j++){
+            dfs(0, j, 0, pac, heights);
+            dfs(m-1, j, 0, atl, heights);
         }
 
-        for(int i =0;i<m;i++){
-            for(int j =0;j<n;j++){
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
                 if(atl[i][j] && pac[i][j]){
-                    ans.push_back({i,j});
+                    ans.push_back({i, j});
                 }
             }
         }
+
         return ans;
     }
 };
