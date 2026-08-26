7eac087f6b34547cd48b91652616dbd7f04a0e72
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-08-26 13:20:38 -0400

    0054-spiral-matrix Medium

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        int top = 0;
-        int bottom = m-1;
         int left = 0;
-        int right = n-1;
+        int right = matrix[0].size()-1;
 
+        int top = 0;
+        int bottom = matrix.size()-1;
         vector<int> ans;
 
-        while(top<=bottom && left<=right){
-            // top side
-            for(int i =left;i<=right;i++){
+        while(left <= right && top <= bottom){
+            for(int i = left; i<= right;i++){
                 ans.push_back(matrix[top][i]);
             }
             top++;
 
-            for(int j = top;j<=bottom;j++){ // right side
-                ans.push_back(matrix[j][right]);
+            for(int i = top; i<= bottom;i++){
+                ans.push_back(matrix[i][right]);
             }
             right--;
 
             if(top<=bottom){
-                for(int i =right;i>=left;i--){
+                for(int i = right;i>= left;i--){
                     ans.push_back(matrix[bottom][i]);
                 }
                 bottom--;
             }
 
             if(left<=right){
-                for(int i =bottom;i>=top;i--){
+                for(int i = bottom ;i>=top;i--){
                     ans.push_back(matrix[i][left]);
                 }
                 left++;
             }
         }
+
         return ans;
+        
     }
 };
