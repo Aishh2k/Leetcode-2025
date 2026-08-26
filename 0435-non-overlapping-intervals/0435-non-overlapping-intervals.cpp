5b0baed1c42c657d5e13280d4ce02852a99116e5
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-08-26 19:05:47 -0400

    0435-non-overlapping-intervals Medium

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int ans = 0;
         sort(intervals.begin(), intervals.end());
         vector<int> cur = intervals[0];
-        int c = 0;
 
-        for(int i =1;i<intervals.size();i++){
-            if(cur[1] > intervals[i][0]){
-                cur[1] =  min(intervals[i][1], cur[1]);
-                c++;
+        for(int i = 1;i<intervals.size();i++){
+            if(cur[1]>intervals[i][0]){
+                ans++;
+                cur[1] = min(cur[1], intervals[i][1]);
             }else{
                 cur = intervals[i];
             }
         }
-        return c;
-
-
+        return ans;
         
     }
 };
+
