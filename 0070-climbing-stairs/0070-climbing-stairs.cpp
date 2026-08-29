92b70628b291d0ed25b2b0e6ea8175d4f04075e2
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-08-29 18:13:12 -0400

    0070-climbing-stairs Easy

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n <=2){
+        if(n<=2){
             return n;
         }
-
-        int prev1 = 1;
-        int prev2 = 2;
+        vector<int> dp(n+1, 0);
+        dp[1] = 1;
+        dp[2] = 2;
 
         for(int i = 3;i<=n;i++){
-            int cur = prev1 + prev2;
-            prev1 = prev2;
-            prev2 = cur;
+            dp[i] = dp[i-1] + dp[i-2];
         }
 
-        return prev2;
+        return dp[n];
+
         
     }
 };
+
+
