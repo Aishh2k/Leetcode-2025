dae4647e8b961adf4b0c55d28117ff6072bd996c
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-08-29 18:26:02 -0400

    0198-house-robber Medium

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if(nums.size() == 1){
+
+        if(nums.size() == 0){
+            return 0;
+        }else if (nums.size() == 1){
             return nums[0];
         }
 
@@ -9,13 +12,12 @@ public:
         dp[0] = nums[0];
         dp[1] = max(nums[0], nums[1]);
 
-        for(int i =2;i<nums.size();i++){
-            int steal = nums[i] + dp[i-2];
-            int skip = dp[i-1];
-
-            dp[i] = max(steal, skip);
+        for(int i = 2;i<nums.size();i++){
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1]);
         }
 
-        return dp[nums.size()-1];
+        return(dp[nums.size()-1]);
+        
     }
 };
+
