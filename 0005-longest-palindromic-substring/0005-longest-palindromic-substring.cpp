59c666bd6c9a32b75ce06e4a43486e87cdcdbf10
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-08-30 13:49:10 -0400

    0005-longest-palindromic-substring Medium

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int r = 0;
-        int l = 0;
-        string res = "";
-        int count = 0;
+        if(s.size()<=1){
+            return s;
+        }
+        int len = 1;
+        string ans = s.substr(0, 1);
 
-        for(int i =0;i<s.size();i++){
-            // odd length palindrome string
+        for(int i = 1;i<s.size();i++){
+            // odd length 
+            int r = i+1;
+            int l = i-1;
+
+            while(l>=0 && r< s.size() && s[r] == s[l]){
+                if(r-l+1 > len){
+                    len = r-l+1;
+                    ans = s.substr(l,len);
+                }
+                r++;
+                l--;
+            }
+
+            //even length
             r = i;
-            l = i;
-
-            while(r < s.size() && l>=0 && s[r] == s[l]){
-                if(r-l+1 >count){
-                    count = r-l+1;
-                    res = s.substr(l, count);
+            l = i-1;
+            while(l>=0 && r<s.size() && s[r] == s[l]){
+                if(r-l+1 > len){
+                    len = r-l+1;
+                    ans = s.substr(l,len);
                 }
                 r++;
                 l--;
             }
-
-            // even
-            l = i;
-            r = i+1;
-
-            while(r< s.size() && l>=0 && s[r] == s[l]){
-                if(r-l+1 >count){
-                    count = r-l+1;
-                    res = s.substr(l, count);
-                }
-                r++;
-                l--;
-            }
-
         }
 
-        return res;
+        return ans;
         
     }
 };
