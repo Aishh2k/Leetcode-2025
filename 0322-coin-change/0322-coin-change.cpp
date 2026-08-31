a2da285b7719bb8e710ce702af8851033632a8c3
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-08-31 11:09:34 -0400

    0322-coin-change Medium

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> total_amount(amount+1, INT_MAX);
-        total_amount[0]=0;
+        if(amount <1){
+            return amount;
+        }
+        vector<int> dp(amount+1, INT_MAX);
+        dp[0] = 0;
 
-        for(int i =1;i<=amount;i++){
-            for(int coin : coins){
-                if(!(coin>i) && total_amount[i-coin] != INT_MAX){
-                    total_amount[i] = min(total_amount[i-coin]+1,total_amount[i]);
+        for(int i = 1;i<=amount;i++){
+            for(int c : coins){
+                if(c<=i && dp[i-c] != INT_MAX){
+                    dp[i] = min(dp[i], dp[i-c] +1);
                 }
             }
         }
-
-        if(total_amount[amount] == INT_MAX){return -1;}
-
-        return(total_amount[amount]);
-
+        if(dp[amount] != INT_MAX){
+            return dp[amount];
+        }else {
+            return -1;
+        }
+        
     }
 };
