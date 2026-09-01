3ed6ee490ad84d5f1887fd6ddb5a9e4c289160ab
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-08-31 23:52:57 -0400

    0139-word-break Medium

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -6,10 +6,10 @@ public:
             st.insert(wordDict[i]);
         }
 
-        vector<bool> dp(s.size()+1,false);
+        vector<bool> dp(s.size()+1, false);
         dp[0] = true;
 
-        for(int i = 1;i<=s.size();i++){
+        for(int i =1;i<=s.size();i++){
             for(int j = i-1;j>=0;j--){
                 if(dp[j] && st.count(s.substr(j, i-j))){
                     dp[i] = true;
