ef6136384abd307ca5a281fe85ed7afb6ae6a216
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-09-02 14:23:09 -0400

    0300-longest-increasing-subsequence Medium

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int i, int prev, vector<vector<int>>& dp) {
-        if (i == nums.size()) {
-            return 0; // no more elements to add so length is 0
-        }
-        // take ith element
-        int take = 0;
-        if (prev != -1 && dp[i][prev] != -1) {
-            return dp[i][prev];
-        }
-
-        if (prev == -1 || nums[prev] < nums[i]) {
-            take = 1 + solve(nums, i + 1, i, dp);
-        }
-
-        // skip ith element
-        int skip = solve(nums, i + 1, prev, dp);
-
-        if(prev != -1){
-            dp[i][prev] = max(skip, take);
-        }
-
-        return max(skip, take);
-    }
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n,-1));
-        return(solve(nums, 0, -1, dp));
+        vector<int> dp(nums.size(), 1);
+        for(int i = 1;i<nums.size();i++){
+            for(int j = 0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i] = max(dp[i], dp[j]+1);
+                }
+            }
+        }
+
+        int ans = *max_element(dp.begin(), dp.end());
+        return ans;
     }
 };
+
+// bottom - up approach
