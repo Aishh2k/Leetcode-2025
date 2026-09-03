2b09f63c7e5c5a0a8369123880cb99c4daa50765
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-09-03 01:20:08 -0400

    0721-accounts-merge Medium

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,56 +1,51 @@
 class Solution {
 public:
-    int find(int node, vector<int>& root){
-        if(root[node] != node){
-            root[node] = find(root[node], root);
+    int find(int a, vector<int>&root){
+        if(root[a] != a){
+            root[a] = find(root[a], root);
         }
-        return root[node];
-    }
-
-    void unite(int a, int b, vector<int>& root){
-        int ra = find(a, root);
-        int rb = find(b, root);
-        if(ra != rb){
-            root[rb] = ra;
-        }
+        return(root[a]);
     }
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
         int n = accounts.size();
-
         vector<int> root(n);
         for(int i =0;i<n;i++){
             root[i] = i;
         }
 
         unordered_map<string, int> emToAcc;
-        for(int i =0;i<n;i++){
-            for(int j =1;j<accounts[i].size();j++){
+        for(int i = 0;i<accounts.size();i++){
+            for(int j = 1;j<accounts[i].size();j++){
                 if(emToAcc.find(accounts[i][j]) == emToAcc.end()){
                     emToAcc[accounts[i][j]] = i;
                 }else{
-                    int prevAcc = emToAcc[accounts[i][j]];
-                    unite(prevAcc, i, root);
+                    int rb = find(i,root);
+                    int ra = find(emToAcc[accounts[i][j]], root);
+                    root[rb] = ra;
                 }
             }
         }
+
         unordered_map<int, vector<string>> accToEm;
 
         for(auto[email, acc]: emToAcc){
-            accToEm[find(acc, root)].push_back(email);
+            accToEm[find(acc,root)].push_back(email);
         }
 
         vector<vector<string>> ans;
 
-        for(auto[acc, emails]: accToEm){
-            sort(emails.begin(), emails.end());
+        for(auto[acc,emails]: accToEm){
             vector<string> temp;
             temp.push_back(accounts[acc][0]);
-
             for(string em: emails){
                 temp.push_back(em);
             }
+            sort(temp.begin()+1, temp.end());
             ans.push_back(temp);
         }
         return ans;
+
+
+        
     }
 };
