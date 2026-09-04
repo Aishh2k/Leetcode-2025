17615debca2c975534332d60dddd82c3cecf4d54
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-09-04 14:05:40 -0400

    0191-number-of-1-bits Easy

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
-    int hammingWeight(uint32_t n) {
-        int res = 0;
-        while(n){
-            res += n%2;
-            n=n/2;
+    int hammingWeight(int n) {
+        int ans = 0;
+
+        while(n != 0){
+            if(n&1){
+                ans++;
+            }
+            n = n >> 1;
         }
-        return res;
+        return ans;
+        
     }
 };
