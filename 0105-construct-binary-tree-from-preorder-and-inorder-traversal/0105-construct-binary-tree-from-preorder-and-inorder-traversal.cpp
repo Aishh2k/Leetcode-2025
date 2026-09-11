8ae89bfd5767afa44c7d3b5109d6007a97252d8b
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-09-11 14:46:26 -0400

    0105-construct-binary-tree-from-preorder-and-inorder-traversal Medium

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -12,10 +12,9 @@
 class Solution {
 public:
     TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int start, int end, int& idx){
-        if(start > end){
+        if(start>end){
             return NULL;
         }
-
         int rootVal = preorder[idx];
         int i;
         for(i = start;i<=end;i++){
@@ -24,14 +23,15 @@ public:
             }
         }
         idx++;
+
         TreeNode* root = new TreeNode(rootVal);
         root->left = solve(preorder, inorder, start, i-1, idx);
         root->right = solve(preorder, inorder, i+1, end, idx);
         return root;
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int n = preorder.size();
+        int n = preorder.size()-1;
         int idx = 0;
-        return solve(preorder, inorder, 0, n-1, idx);
+        return solve(preorder, inorder, 0, n, idx);
     }
 };
