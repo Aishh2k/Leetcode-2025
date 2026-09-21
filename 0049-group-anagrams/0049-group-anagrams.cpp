6e565111de02e1b5e09d251b6d1d2a06380c1f00
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-09-20 22:48:14 -0400

    0049-group-anagrams Medium

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> res;
         unordered_map<string, vector<string>> mp;
 
         for(string s: strs){
@@ -9,19 +8,20 @@ public:
             for(char c: s){
                 count[c-'a']++;
             }
-
             string key = "";
             for(int i = 0;i<26;i++){
-                key += to_string(count[i]) + "#";
+                key = key + to_string(count[i]) + "#";
             }
+
             mp[key].push_back(s);
         }
 
-        for( auto i: mp){
-            res.push_back(i.second);
+        vector<vector<string>> ans;
+
+        for(auto i: mp){
+            ans.push_back(i.second);
         }
 
-        return res;
-        
+        return ans;
     }
 };
