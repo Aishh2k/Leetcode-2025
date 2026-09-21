7485459ec5cc44f06aec03b773bed50dcded86ca
Aiswarya Jayachandran <42784969+Aishh2k@users.noreply.github.com>
2026-09-20 23:06:59 -0400

    0238-product-of-array-except-self Medium

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,31 +1,38 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> right(n, 1);
-        vector<int> left(n, 1);
+        vector<int> right(nums.size(), 1);
+        vector<int> left(nums.size(), 1);
 
-        for(int i = n-2;i>=0 ;i--){
+        for(int i =1 ;i<nums.size();i++){
+            left[i] = nums[i-1] * left[i-1];
+        }
+
+        for(int i = nums.size()-2;i>=0;i--){
             right[i] = nums[i+1] * right[i+1];
         }
 
-        for(int i =1;i<n;i++){
-            left[i] = nums[i-1] * left[i-1];
-        }
-        vector<int> res(n,0);
-        for(int i =0;i<n;i++){
-            res[i] = left[i] * right[i];
+        vector<int> prod(nums.size(), 0);
+
+        for(int i = 0;i <nums.size();i++){
+            prod[i] = left[i] * right[i];
         }
 
-        return res;
-
-        
-        
+        return prod;
     }
 };
 
 /*
 
-a b c d
-right = bcd cd d 1
-left = 1 a ab abc */
+a    b    c    d
+
+left
+1   a    ab  abc
+
+right
+bcd  cd  d    1
+
+bcd  acd  abd  abc  -> result
+
+
+*/
